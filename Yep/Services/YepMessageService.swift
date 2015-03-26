11cbad90a1e5ee87086a3f4fb58d76b3378faa88
kevinzhow <kevinchou.c@gmail.com>
2015-03-26 22:13:58 +0800

    Add Faye

---
+++ Yep/Services/YepMessageService.swift
@@ -0,0 +1,14 @@
+//
+//  YepMessageService.swift
+//  Yep
+//
+//  Created by kevinzhow on 15/3/26.
+//  Copyright (c) 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+import MZFayeClient
+
+class YepMessageService: NSObject {
+   
+}
