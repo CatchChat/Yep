012e31ff886b7670e3b7ced1cd8956f55b8ff942
kevinzhow <kevinchou.c@gmail.com>
2015-03-28 02:09:15 +0800

    Fix tabbar black shadow

---
+++ Yep/ViewControllers/TabBar/YepTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  YepTabBarController.swift
+//  Yep
+//
+//  Created by kevinzhow on 15/3/28.
+//  Copyright (c) 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class YepTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.whiteColor()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
