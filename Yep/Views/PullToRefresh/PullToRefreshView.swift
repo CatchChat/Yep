c75832bf94dbed3f35dd22a708315423a8e36ab3
nixzhu <zhuhongxu@gmail.com>
2015-04-14 15:08:26 +0800

    fans animation in PullToRefreshView

--- Yep/Views/PullToRefresh/PullToRefreshView.swift
+++ Yep/Views/PullToRefresh/PullToRefreshView.swift
@@ -13,23 +13,60 @@ protocol PullToRefreshViewDelegate: class {
     func scrollView() -> UIScrollView
 }
 
-private let sceneHeight: CGFloat = 100
+private let sceneHeight: CGFloat = 80
 
 class PullToRefreshView: UIView {
 
     var progressPercentage: CGFloat = 0
+
     weak var delegate: PullToRefreshViewDelegate?
 
     var isRefreshing = false
 
+    var refreshItems = [RefreshItem]()
+
     override func didMoveToSuperview() {
         super.didMoveToSuperview()
-        
-        updateBackgroundColor()
+
+        self.clipsToBounds = true
+
+        setupRefreshItems()
+
+        updateColors()
     }
 
-    func updateBackgroundColor() {
-        backgroundColor = UIColor(white: (1 - progressPercentage) * 0.7, alpha: 1.0)
+    func setupRefreshItems() {
+        let logoImageView = UIImageView(image: UIImage(named: "fans"))
+
+        refreshItems = [
+            RefreshItem(
+                view: logoImageView,
+                centerEnd: CGPoint(
+                    x: CGRectGetMidX(UIScreen.mainScreen().bounds),
+                    y: 200 - sceneHeight * 0.5
+                ),
+                parallaxRatio: 0,
+                sceneHeight: sceneHeight
+            ),
+        ]
+
+        for refreshItem in refreshItems {
+            addSubview(refreshItem.view)
+        }
+    }
+
+    func updateColors() {
+        backgroundColor = UIColor(white: (1 - progressPercentage) * 1.0, alpha: 1.0)
+
+        for refreshItem in refreshItems {
+            refreshItem.updateViewTintColor(UIColor.yepTintColor().colorWithAlphaComponent(progressPercentage))
+        }
+    }
+
+    func updateRefreshItemPositions() {
+        for refreshItem in refreshItems {
+            refreshItem.updateViewPositionForPercentage(progressPercentage)
+        }
     }
 
     // MARK: Actions
@@ -51,6 +88,10 @@ class PullToRefreshView: UIView {
             self.isRefreshing = false
 
             furtherAction()
+
+            for refreshItem in self.refreshItems {
+                refreshItem.view.layer.removeAllAnimations()
+            }
         })
     }
 }
@@ -65,7 +106,9 @@ extension PullToRefreshView: UIScrollViewDelegate {
         let refreshViewVisibleHeight = max(0, -(scrollView.contentOffset.y + scrollView.contentInset.top))
         progressPercentage = min(1, refreshViewVisibleHeight / sceneHeight)
 
-        updateBackgroundColor()
+        updateRefreshItemPositions()
+
+        updateColors()
     }
 
     func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
@@ -76,6 +119,55 @@ extension PullToRefreshView: UIScrollViewDelegate {
             targetContentOffset.memory.y = -scrollView.contentInset.top
 
             delegate?.pulllToRefreshViewDidRefresh(self)
+
+            for refreshItem in refreshItems {
+
+                CATransaction.begin()
+
+                let angle = CGFloat(M_PI * 2)
+
+                let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+                rotationAnimation.repeatCount = 4
+                rotationAnimation.byValue = angle
+                rotationAnimation.duration = 0.75
+                rotationAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+                rotationAnimation.removedOnCompletion = true
+
+                CATransaction.setCompletionBlock({ () -> Void in
+                    refreshItem.view.transform = CGAffineTransformRotate(refreshItem.view.transform, angle)
+                })
+
+                refreshItem.view.layer.addAnimation(rotationAnimation, forKey: "rotationAnimation")
+
+                CATransaction.commit()
+            }
         }
+    }
+}
+
+class RefreshItem {
+
+    unowned var view: UIView
+
+    private var centerStart: CGPoint
+    private var centerEnd: CGPoint
+
+    init(view: UIView, centerEnd: CGPoint, parallaxRatio: CGFloat, sceneHeight: CGFloat) {
+        self.view = view
+        self.centerEnd = centerEnd
+
+        centerStart = CGPoint(x: centerEnd.x, y: centerEnd.y + (parallaxRatio * sceneHeight))
+        self.view.center = centerStart
+    }
+
+    func updateViewPositionForPercentage(percentage: CGFloat) {
+        view.center = CGPoint(
+            x: centerStart.x + (centerEnd.x - centerStart.x) * percentage,
+            y: centerStart.y + (centerEnd.y - centerStart.y) * percentage
+        )
+    }
+
+    func updateViewTintColor(tintColor: UIColor) {
+        view.tintColor = tintColor
     }
 }
