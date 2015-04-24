c006c24d93236cd151a903785e502234f5dae278
nixzhu <zhuhongxu@gmail.com>
2015-04-24 14:32:39 +0800

    add MediaView

---
+++ Yep/Views/Media/MediaView.swift
@@ -0,0 +1,51 @@
+//
+//  MediaView.swift
+//  Yep
+//
+//  Created by NIX on 15/4/24.
+//  Copyright (c) 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class MediaView: UIView {
+
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .ScaleAspectFill
+        return imageView
+        }()
+
+    lazy var videoPlayerLayer: AVPlayerLayer = {
+        let player = AVPlayer()
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.bounds
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        return playerLayer
+        }()
+
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+
+        makeUI()
+    }
+
+    func makeUI() {
+
+        addSubview(imageView)
+
+        imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+
+        let viewsDictionary = [
+            "imageView": imageView,
+        ]
+
+        let constraintsV = NSLayoutConstraint.constraintsWithVisualFormat("V:|[imageView]|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
+
+        let constraintsH = NSLayoutConstraint.constraintsWithVisualFormat("H:|[imageView]|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
+
+        NSLayoutConstraint.activateConstraints(constraintsV)
+        NSLayoutConstraint.activateConstraints(constraintsH)
+    }
+}
