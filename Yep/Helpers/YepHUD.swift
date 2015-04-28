c8980673c9c1f079f2a1a0fbc84a5d24beaf8fca
nixzhu <zhuhongxu@gmail.com>
2015-04-29 00:30:59 +0800

    better animation of hide YepHUD

--- Yep/Helpers/YepHUD.swift
+++ Yep/Helpers/YepHUD.swift
@@ -13,6 +13,7 @@ class YepHUD: NSObject {
     static let sharedInstance = YepHUD()
 
     var isShowing = false
+    var dismissTimer: NSTimer?
 
     lazy var containerView: UIView = {
         let view = UIView()
@@ -56,14 +57,20 @@ class YepHUD: NSObject {
                         self.sharedInstance.activityIndicator.center = self.sharedInstance.containerView.center
                         self.sharedInstance.activityIndicator.startAnimating()
 
+                        self.sharedInstance.activityIndicator.alpha = 0
                         self.sharedInstance.activityIndicator.transform = CGAffineTransformMakeScale(0.0001, 0.0001)
                         UIView.animateWithDuration(0.2, delay: 0.0, options: UIViewAnimationOptions(0), animations: { () -> Void in
                             self.sharedInstance.activityIndicator.transform = CGAffineTransformMakeScale(1.0, 1.0)
+                            self.sharedInstance.activityIndicator.alpha = 1
 
                         }, completion: { (finished) -> Void in
                             self.sharedInstance.activityIndicator.transform = CGAffineTransformIdentity
 
-                            let dismissTimer = NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector: "forcedHideActivityIndicator", userInfo: nil, repeats: false)
+                            if let dismissTimer = self.sharedInstance.dismissTimer {
+                                dismissTimer.invalidate()
+                            }
+                            
+                            self.sharedInstance.dismissTimer = NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector: "forcedHideActivityIndicator", userInfo: nil, repeats: false)
                         })
                     })
             }
@@ -90,8 +97,12 @@ class YepHUD: NSObject {
         if self.sharedInstance.isShowing {
 
             dispatch_async(dispatch_get_main_queue()) {
+
+                self.sharedInstance.activityIndicator.transform = CGAffineTransformIdentity
+
                 UIView.animateWithDuration(0.2, delay: 0.0, options: UIViewAnimationOptions(0), animations: { () -> Void in
                     self.sharedInstance.activityIndicator.transform = CGAffineTransformMakeScale(0.0001, 0.0001)
+                    self.sharedInstance.activityIndicator.alpha = 0
 
                 }, completion: { (finished) -> Void in
                     self.sharedInstance.activityIndicator.removeFromSuperview()
