675fe302dafb6dbdd0dd7e65dc272dcf624dc82b
nixzhu <zhuhongxu@gmail.com>
2015-05-04 21:21:03 +0800

    when user tap on map, show UserPickedLocationPin

--- Yep/ViewControllers/PickLocation/PickLocationViewController.swift
+++ Yep/ViewControllers/PickLocation/PickLocationViewController.swift
@@ -16,6 +16,9 @@ class PickLocationViewController: UIViewController {
 
     var isFirstShowUserLocation = true
 
+    var userPickedLocationPin: UserPickedLocationPin?
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,6 +28,9 @@ class PickLocationViewController: UIViewController {
         let location = YepLocationService.sharedManager.locationManager.location
         let region = MKCoordinateRegionMakeWithDistance(location.coordinate, 20000, 20000)
         mapView.setRegion(region, animated: false)
+
+        let tap = UITapGestureRecognizer(target: self, action: "addAnnotation:")
+        mapView.addGestureRecognizer(tap)
     }
 
 
@@ -39,6 +45,20 @@ class PickLocationViewController: UIViewController {
             // TODO: send location
         })
     }
+
+    func addAnnotation(sender: UITapGestureRecognizer) {
+
+        mapView.removeAnnotation(userPickedLocationPin)
+
+        let point = sender.locationInView(mapView)
+        let coordinate = mapView.convertPoint(point, toCoordinateFromView: mapView)
+
+        let pin = UserPickedLocationPin(title: "Pin", subtitle: "User Picked Location", coordinate: coordinate)
+        mapView.addAnnotation(pin)
+
+        userPickedLocationPin = pin
+
+    }
 }
 
 extension PickLocationViewController: MKMapViewDelegate {
@@ -52,6 +72,30 @@ extension PickLocationViewController: MKMapViewDelegate {
             let region = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
             mapView.setRegion(region, animated: true)
         }
+    }
+
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+
+        if let annotation = annotation as? UserPickedLocationPin {
+
+            let identifier = "UserPickedLocationPinView"
+
+            if let annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier) {
+                annotationView.annotation = annotation
+
+                return annotationView
+
+            } else {
+                let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView.image = UIImage(named: "icon_pin")
+                annotationView.enabled = false
+                annotationView.canShowCallout = false
+
+                return annotationView
+            }
+        }
+
+        return nil
     }
 }
 
---
+++ Yep/ViewControllers/PickLocation/UserPickedLocationPin.swift
@@ -0,0 +1,21 @@
+//
+//  UserPickLocationAnnotation.swift
+//  Yep
+//
+//  Created by NIX on 15/5/4.
+//  Copyright (c) 2015年 Catch Inc. All rights reserved.
+//
+
+import MapKit
+
+class UserPickedLocationPin: NSObject, MKAnnotation {
+    let title: String
+    let subtitle: String
+    let coordinate: CLLocationCoordinate2D
+
+    init(title: String, subtitle: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.subtitle = subtitle
+        self.coordinate = coordinate
+    }
+}
