a355a61e3a96bf47b75dbc0db731437cfc3cda79
nixzhu <zhuhongxu@gmail.com>
2015-05-12 16:24:41 +0800

    add DribbbleShotCell

--- Yep/ViewControllers/SocialWorks/SocialWorkDribbbleViewController.swift
+++ Yep/ViewControllers/SocialWorks/SocialWorkDribbbleViewController.swift
@@ -10,10 +10,47 @@ import UIKit
 
 class SocialWorkDribbbleViewController: UIViewController {
 
+    @IBOutlet weak var dribbbleCollectionView: UICollectionView!
+
+    let dribbbleShotCellIdentifier = "DribbbleShotCell"
+
+    lazy var collectionViewWidth: CGFloat = {
+        return CGRectGetWidth(self.dribbbleCollectionView.bounds)
+        }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        dribbbleCollectionView.registerNib(UINib(nibName: dribbbleShotCellIdentifier, bundle: nil), forCellWithReuseIdentifier: dribbbleShotCellIdentifier)
+
     }
 
+}
+
+extension SocialWorkDribbbleViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(dribbbleShotCellIdentifier, forIndexPath: indexPath) as! DribbbleShotCell
+
+        cell.imageView.image = UIImage(named: "Cover3")
+
+        return cell
+    }
+
+    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize {
+
+        let width = collectionViewWidth * 0.5
+        let height = width
+
+        return CGSizeMake(width, height)
+    }
 }
---
+++ Yep/Views/Cells/DribbbleShot/DribbbleShotCell.swift
@@ -0,0 +1,21 @@
+//
+//  DribbbleShotCell.swift
+//  Yep
+//
+//  Created by NIX on 15/5/12.
+//  Copyright (c) 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class DribbbleShotCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
