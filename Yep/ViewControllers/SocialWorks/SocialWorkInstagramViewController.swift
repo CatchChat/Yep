eede229ff8b98354f73f96b40da2b06eb036f4f2
nixzhu <zhuhongxu@gmail.com>
2015-05-14 14:41:42 +0800

    add SocialWorkInstagramViewController

---
+++ Yep/ViewControllers/SocialWorks/SocialWorkInstagramViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SocialWorkInstagramViewController.swift
+//  Yep
+//
+//  Created by NIX on 15/5/14.
+//  Copyright (c) 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class SocialWorkInstagramViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
