ca9b41ab4ef5ff9136c7c80ecea3a99873fbf060
nixzhu <zhuhongxu@gmail.com>
2015-05-14 17:04:47 +0800

    add InstagramMediaCell

--- Yep/ViewControllers/SocialWorks/SocialWorkInstagramViewController.swift
+++ Yep/ViewControllers/SocialWorks/SocialWorkInstagramViewController.swift
@@ -10,10 +10,67 @@ import UIKit
 
 class SocialWorkInstagramViewController: UIViewController {
 
+    var socialAccount: SocialAccount?
+    var profileUser: ProfileUser?
+
+
+    @IBOutlet weak var instagramCollectionView: UICollectionView!
+
+    let instagramMediaCellIdentifier = "InstagramMediaCell"
+
+    lazy var collectionViewWidth: CGFloat = {
+        return CGRectGetWidth(self.instagramCollectionView.bounds)
+        }()
+
+    var dribbbleShots = Array<DribbbleWork.Shot>() {
+        didSet {
+            updateInstagramCollectionView()
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        instagramCollectionView.registerNib(UINib(nibName: instagramMediaCellIdentifier, bundle: nil), forCellWithReuseIdentifier: instagramMediaCellIdentifier)
+
     }
 
+    // MARK: Actions
+
+    func updateInstagramCollectionView() {
+        instagramCollectionView.reloadData()
+    }
+
+}
+
+extension SocialWorkInstagramViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(instagramMediaCellIdentifier, forIndexPath: indexPath) as! InstagramMediaCell
+
+        cell.imageView.image = UIImage(named: "Cover3")!
+
+        return cell
+    }
+
+    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize {
+
+        let width = collectionViewWidth * 0.5
+        let height = width
+
+        return CGSizeMake(width, height)
+    }
+
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+
+    }
 }
