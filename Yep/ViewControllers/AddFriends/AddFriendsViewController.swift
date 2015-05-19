54da9a01574aa94ba0b0723aef7aa5b2212e2ea1
nixzhu <zhuhongxu@gmail.com>
2015-05-19 14:47:24 +0800

    add AddFriendSearchCell

--- Yep/ViewControllers/AddFriends/AddFriendsViewController.swift
+++ Yep/ViewControllers/AddFriends/AddFriendsViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class AddFriendsViewController: UIViewController {
 
+    @IBOutlet weak var addFriendsTableView: UITableView!
+
+    let addFriendSearchCellIdentifier = "AddFriendSearchCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = NSLocalizedString("Add Friends", comment: "")
+
+
+        addFriendsTableView.registerNib(UINib(nibName: addFriendSearchCellIdentifier, bundle: nil), forCellReuseIdentifier: addFriendSearchCellIdentifier)
     }
 
     // MARK: Actions
@@ -21,5 +28,20 @@ class AddFriendsViewController: UIViewController {
     @IBAction func done(sender: UIBarButtonItem) {
         dismissViewControllerAnimated(true, completion: nil)
         // TODO: done add friend
+    }
+}
+
+extension AddFriendsViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(addFriendSearchCellIdentifier) as! AddFriendSearchCell
+        return cell
     }
 }
