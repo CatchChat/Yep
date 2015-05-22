8cf8028b1cc1f42ad69c23f589fd353649917e23
nixzhu <zhuhongxu@gmail.com>
2015-05-22 16:11:45 +0800

    add SearchedUsersViewController

---
+++ Yep/ViewControllers/SearchedUsers/SearchedUsersViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchedUsersViewController.swift
+//  Yep
+//
+//  Created by NIX on 15/5/22.
+//  Copyright (c) 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class SearchedUsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = NSLocalizedString("Searched Users", comment: "")
+    }
+
+}
