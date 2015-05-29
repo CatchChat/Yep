6b64b909c1283f558e41267e2548109724a689e1
kevinzhow <kevinchou.c@gmail.com>
2015-05-30 01:13:19 +0800

    add chat state cell

---
+++ Yep/Views/Cells/ChatState/ChatStateCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChatStateCell.swift
+//  Yep
+//
+//  Created by kevinzhow on 15/5/30.
+//  Copyright (c) 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class ChatStateCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
