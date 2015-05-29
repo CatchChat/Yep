254daee3f30213d05e77d071c9eb4f50378ac5ef
kevinzhow <kevinchou.c@gmail.com>
2015-05-30 06:48:40 +0800

    Fix app badge

--- Yep/AppDelegate.swift
+++ Yep/AppDelegate.swift
@@ -88,6 +88,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        
+        UIApplication.sharedApplication().applicationIconBadgeNumber = 0
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
@@ -110,6 +112,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
 
         isColdLaunch = false
+    
     }
 
     func applicationWillTerminate(application: UIApplication) {
