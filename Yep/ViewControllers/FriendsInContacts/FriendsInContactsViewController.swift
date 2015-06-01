0de7e6ab501db73a3dbc77b1781280efe7ded074
nixzhu <zhuhongxu@gmail.com>
2015-06-01 16:56:01 +0800

    use APAddressBook to loadContacts

--- Yep/ViewControllers/FriendsInContacts/FriendsInContactsViewController.swift
+++ Yep/ViewControllers/FriendsInContacts/FriendsInContactsViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import APAddressBook
 
 class FriendsInContactsViewController: UIViewController {
+
+    lazy var addressBook: APAddressBook = {
+        let addressBook = APAddressBook()
+        addressBook.fieldsMask = APContactField(rawValue: APContactField.CompositeName.rawValue | APContactField.Phones.rawValue)
+        return addressBook
+        }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = NSLocalizedString("Available Friends", comment: "")
+
+
+        addressBook.loadContacts{ (contacts: [AnyObject]!, error: NSError!) in
+            if let contacts = contacts as? [APContact] {
+                for contact in contacts {
+                    println(contact.compositeName, contact.phones)
+                }
+
+            } else if (error != nil) {
+                let alert = UIAlertView(title: "Error", message: error.localizedDescription,
+                    delegate: nil, cancelButtonTitle: "OK")
+                alert.show()
+            }
+        }
     }
 
 }
