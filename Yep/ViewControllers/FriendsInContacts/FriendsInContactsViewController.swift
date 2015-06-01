ae53166cb97ec05a0aa9d5a1d8899aed4010f37b
nixzhu <zhuhongxu@gmail.com>
2015-06-01 23:27:08 +0800

    fix bars

--- Yep/ViewControllers/FriendsInContacts/FriendsInContactsViewController.swift
+++ Yep/ViewControllers/FriendsInContacts/FriendsInContactsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import APAddressBook
 
-class FriendsInContactsViewController: UIViewController {
+class FriendsInContactsViewController: BaseViewController {
 
     @IBOutlet weak var friendsTableView: UITableView!
 
