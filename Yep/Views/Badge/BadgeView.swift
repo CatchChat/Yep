cf6935da008bbcd58e40028c1116a0c4b8782358
nixzhu <zhuhongxu@gmail.com>
2015-07-02 20:46:44 +0800

    BadgeView colors

--- Yep/Views/Badge/BadgeView.swift
+++ Yep/Views/Badge/BadgeView.swift
@@ -29,6 +29,51 @@ class BadgeView: UIView {
         case Game = "game"
         case Ball = "ball"
         case Tech = "tech"
+
+        struct Color {
+            static let red = UIColor(red: 1, green: 56/255.0, blue: 36/255.0, alpha: 1)
+            static let green = UIColor(red:0.1, green:0.74, blue:0.61, alpha:1)
+            static let blue = UIColor(red: 56/255.0, green: 166/255.0, blue: 249/255.0, alpha: 1)
+            static let yellow = UIColor(red: 245/255.0, green: 166/255.0, blue: 35/255.0, alpha: 1)
+            static let dark = UIColor(red: 52/255.0, green: 73/255.0, blue: 94/255.0, alpha: 1)
+        }
+
+        var color: UIColor {
+            switch self {
+            case .Palette:
+                return Color.red
+            case .Plane:
+                return Color.blue
+            case .Heart:
+                return Color.red
+            case .Star:
+                return Color.yellow
+            case .Bubble:
+                return Color.dark
+
+            case .Android:
+                return Color.green
+            case .Apple:
+                return Color.dark
+            case .Pet:
+                return Color.yellow
+            case .Wine:
+                return Color.red
+            case .Music:
+                return Color.green
+
+            case .Steve:
+                return Color.blue
+            case .Camera:
+                return Color.green
+            case .Game:
+                return Color.dark
+            case .Ball:
+                return Color.blue
+            case .Tech:
+                return Color.blue
+            }
+        }
     }
 
     //@IBInspectable
@@ -42,10 +87,8 @@ class BadgeView: UIView {
 
     var enabled: Bool = false {
         willSet {
-            //backgroundView.backgroundColor = newValue ? UIColor.yepTintColor() : UIColor.clearColor()
-
             UIView.animateWithDuration(0.1, delay: 0.0, options: .CurveEaseInOut, animations: { _ in
-                self.badgeImageView.tintColor = newValue ? UIColor.whiteColor() : UIColor.yepTintColor()
+                self.badgeImageView.tintColor = newValue ? UIColor.whiteColor() : self.badge.color
             }, completion: { finished in
             })
         }
