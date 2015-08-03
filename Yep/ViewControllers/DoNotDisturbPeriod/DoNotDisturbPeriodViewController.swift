2dbb3a1f8d171db7dca313a6cb9b88942e43ae95
nixzhu <zhuhongxu@gmail.com>
2015-08-03 15:55:14 +0800

    basic UI of DoNotDisturbPeriodViewController

--- Yep/ViewControllers/DoNotDisturbPeriod/DoNotDisturbPeriodViewController.swift
+++ Yep/ViewControllers/DoNotDisturbPeriod/DoNotDisturbPeriodViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class DoNotDisturbPeriodViewController: UIViewController {
 
+    @IBOutlet weak var fromButton: UIButton!
+
+    @IBOutlet weak var toButton: UIButton!
+
+    @IBOutlet weak var pickerView: UIPickerView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = NSLocalizedString("Mute", comment: "")
+
+        fromButton.backgroundColor = UIColor.lightGrayColor().colorWithAlphaComponent(0.5)
+        toButton.backgroundColor = UIColor.whiteColor()
     }
+}
+
+// MARK: - UIPickerViewDataSource, UIPickerViewDelegate
+
+extension DoNotDisturbPeriodViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 24
+        } else if component == 1 {
+            return 60
+        }
+
+        return 0
+    }
+
+    func pickerView(pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return 60
+    }
+
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return "\(row + 1)"
+    }
+
+
 }
