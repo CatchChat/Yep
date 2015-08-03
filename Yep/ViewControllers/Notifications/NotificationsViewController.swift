365a0325db2811418a62c11a32c45453428b0324
nixzhu <zhuhongxu@gmail.com>
2015-08-03 14:07:06 +0800

    add NotificationsViewController

---
+++ Yep/ViewControllers/Notifications/NotificationsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationsViewController.swift
+//  Yep
+//
+//  Created by NIX on 15/8/3.
+//  Copyright (c) 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = NSLocalizedString("Notifications", comment: "")
+    }
+}
