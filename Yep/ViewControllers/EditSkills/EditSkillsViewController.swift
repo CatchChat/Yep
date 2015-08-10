d230263233ab8b165cb94f63cbee8f89e35a4c49
nixzhu <zhuhongxu@gmail.com>
2015-08-10 16:35:24 +0800

    basic UI of EditSkillsViewController

--- Yep/ViewControllers/EditSkills/EditSkillsViewController.swift
+++ Yep/ViewControllers/EditSkills/EditSkillsViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class EditSkillsViewController: BaseViewController {
 
+
+    @IBOutlet weak var skillsTableView: UITableView!
+
+    @IBOutlet weak var addSkillsView: BottomButtonView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         animatedOnNavigationBar = false
 
         title = NSLocalizedString("Edit Skills", comment: "")
+
+        var contentInset = skillsTableView.contentInset
+        contentInset.bottom = addSkillsView.frame.height
+        skillsTableView.contentInset = contentInset
     }
 }
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+
+extension EditSkillsViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        let cell = UITableViewCell()
+
+        cell.textLabel?.text = "Hello"
+
+        return cell
+    }
+}
+
