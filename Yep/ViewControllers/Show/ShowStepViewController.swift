db6a0034f4111222f158288a96dfe62856497bf2
nixzhu <zhuhongxu@gmail.com>
2015-08-12 18:41:45 +0800

    add ShowViewController & ShowStepViewController

---
+++ Yep/ViewControllers/Show/ShowStepViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ShowStepViewController.swift
+//  Yep
+//
+//  Created by nixzhu on 15/8/12.
+//  Copyright (c) 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class ShowStepViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
