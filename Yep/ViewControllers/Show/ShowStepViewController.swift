3aa5470e9c13224d68c5385db6e4b9bee889b3d3
nixzhu <zhuhongxu@gmail.com>
2015-08-20 12:15:43 +0800

    remove old show

--- Yep/ViewControllers/Show/ShowStepViewController.swift
+++ Yep/ViewControllers/Show/ShowStepViewController.swift
@@ -12,24 +12,7 @@ class ShowStepViewController: UIViewController {
 
     var showName: String?
 
-    @IBOutlet weak var webView: UIWebView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        loadHTML()
-    }
-
-    private func loadHTML() {
-
-        if let htmlName = showName {
-            if let
-                htmlPath = NSBundle.mainBundle().pathForResource(htmlName, ofType: "html", inDirectory: "ShowResources"),
-                string = String(contentsOfFile: htmlPath, encoding: NSUTF8StringEncoding, error: nil),
-                baseURL = NSURL(fileURLWithPath: htmlPath) {
-
-                    webView.loadHTMLString(string, baseURL: baseURL)
-            }
-        }
     }
 }
