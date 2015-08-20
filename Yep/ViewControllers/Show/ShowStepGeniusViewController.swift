469ba570c548236568a5a2df02d1045358cdfb90
nixzhu <zhuhongxu@gmail.com>
2015-08-20 13:35:20 +0800

    first try animate dot1

--- Yep/ViewControllers/Show/ShowStepGeniusViewController.swift
+++ Yep/ViewControllers/Show/ShowStepGeniusViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ShowStepGeniusViewController: UIViewController {
 
+    @IBOutlet weak var dot1: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        animateView(dot1, alongWithPath: UIBezierPath(ovalInRect: dot1.frame), duration: 4)
+    }
+
+    func animateView(view: UIView, alongWithPath path: UIBezierPath, duration: CFTimeInterval) {
+
+        let animation = CAKeyframeAnimation(keyPath: "position")
+
+        animation.calculationMode = kCAAnimationPaced
+        animation.fillMode = kCAFillModeForwards
+        animation.duration = duration
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.repeatCount = Float.infinity
+
+        animation.path = path.CGPath
+
+        view.layer.addAnimation(animation, forKey: "Animation")
     }
 }
+
