68f1fbbcc2a7cf8ee3663161748229bd6614c98c
nixzhu <zhuhongxu@gmail.com>
2015-08-20 16:18:45 +0800

    layout of ShowStepMeetViewController

--- Yep/ViewControllers/Show/ShowStepMeetViewController.swift
+++ Yep/ViewControllers/Show/ShowStepMeetViewController.swift
@@ -10,26 +10,13 @@ import UIKit
 
 class ShowStepMeetViewController: UIViewController {
 
+    @IBOutlet weak var yellowTriangle: UIImageView!
+    @IBOutlet weak var greenTriangle: UIImageView!
+    @IBOutlet weak var purpleTriangle: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
