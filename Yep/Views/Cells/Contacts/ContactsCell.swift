3ad15166c7477ece59da7a71a4ca71235f1b23d3
nixzhu <zhuhongxu@gmail.com>
2015-08-24 15:47:55 +0800

    refactor config ContactsCell

--- Yep/Views/Cells/Contacts/ContactsCell.swift
+++ Yep/Views/Cells/Contacts/ContactsCell.swift
@@ -26,5 +26,35 @@ class ContactsCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
+
+    func configureWithDiscoveredUser(discoveredUser: DiscoveredUser, tableView: UITableView, indexPath: NSIndexPath) {
+
+        let radius = min(CGRectGetWidth(avatarImageView.bounds), CGRectGetHeight(avatarImageView.bounds)) * 0.5
+
+        let avatarURLString = discoveredUser.avatarURLString
+        AvatarCache.sharedInstance.roundAvatarWithAvatarURLString(avatarURLString, withRadius: radius) { [weak self] roundImage in
+            dispatch_async(dispatch_get_main_queue()) {
+                if let _ = tableView.cellForRowAtIndexPath(indexPath) {
+                    self?.avatarImageView.image = roundImage
+                }
+            }
+        }
+
+        joinedDateLabel.text = discoveredUser.introduction
+
+        if let distance = discoveredUser.distance?.format(".1") {
+            lastTimeSeenLabel.text = "\(distance)km | \(NSDate(timeIntervalSince1970: discoveredUser.lastSignInUnixTime).timeAgo)"
+        } else {
+            lastTimeSeenLabel.text = "\(NSDate(timeIntervalSince1970: discoveredUser.lastSignInUnixTime).timeAgo)"
+        }
+
+        nameLabel.text = discoveredUser.nickname
+
+        if let badgeName = discoveredUser.badge, badge = BadgeView.Badge(rawValue: badgeName) {
+            badgeImageView.image = badge.image
+            badgeImageView.tintColor = badge.color
+        } else {
+            badgeImageView.image = nil
+        }
+    }
 }
