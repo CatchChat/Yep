1bf12b69b34f087bba67b791785d10bada6c907d
nixzhu <zhuhongxu@gmail.com>
2015-08-28 17:21:34 +0800

    add tableView

--- Yep/ViewControllers/About/AboutViewController.swift
+++ Yep/ViewControllers/About/AboutViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var aboutTableView: UITableView!
+    @IBOutlet weak var aboutTableViewHeightConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = NSLocalizedString("About", comment: "")
+    }
+}
+
+extension AboutViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
 }
