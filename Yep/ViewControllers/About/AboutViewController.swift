b0bc092d1e71609bc72a757b79802ed3c721dca5
nixzhu <zhuhongxu@gmail.com>
2015-08-28 17:14:49 +0800

    add AboutViewController

---
+++ Yep/ViewControllers/About/AboutViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AboutViewController.swift
+//  Yep
+//
+//  Created by nixzhu on 15/8/28.
+//  Copyright (c) 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = NSLocalizedString("About", comment: "")
+    }
+}
