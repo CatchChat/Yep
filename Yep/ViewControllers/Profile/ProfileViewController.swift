00a65db991bd02785b72d32708cb79b4088c28da
nixzhu <zhuhongxu@gmail.com>
2015-09-22 17:32:04 +0800

    FeedbackTextView

--- Yep/ViewControllers/Profile/ProfileViewController.swift
+++ Yep/ViewControllers/Profile/ProfileViewController.swift
@@ -646,10 +646,6 @@ class ProfileViewController: UIViewController {
                 })
             }
 
-            YepAlert.confirmOrCancel(title: NSLocalizedString("Notice", comment: ""), message: NSLocalizedString("You don't have any skills!\nWould you like to pick some?", comment: ""), confirmTitle: NSLocalizedString("OK", comment: ""), cancelTitle: NSLocalizedString("Not now", comment: ""), inViewController: self, withConfirmAction: { [weak self] in
-                self?.pickSkills()
-                }, cancelAction: {})
-            
             if profileUserIsMe {
 
                 // 提示没有 Skills
