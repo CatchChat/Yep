5adb327fa87d5ff6087710fc4667d1aecdae42f9
nixzhu <zhuhongxu@gmail.com>
2015-09-25 11:49:48 +0800

    feedsCollectionView

--- Yep/ViewControllers/Feeds/FeedsViewController.swift
+++ Yep/ViewControllers/Feeds/FeedsViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class FeedsViewController: UIViewController {
 
+    @IBOutlet weak var feedsCollectionView: UICollectionView!
+
+    lazy var collectionViewWidth: CGFloat = {
+        return CGRectGetWidth(self.feedsCollectionView.bounds)
+        }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = NSLocalizedString("Feeds", comment: "")
+
+        feedsCollectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
 
     // MARK: - Navigation
@@ -21,3 +29,30 @@ class FeedsViewController: UIViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     }
 }
+
+extension FeedsViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath)
+        cell.backgroundColor = UIColor.redColor()
+        return cell
+    }
+
+    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize {
+
+        return CGSize(width: collectionViewWidth, height: 200)
+    }
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+    }
+}
+
