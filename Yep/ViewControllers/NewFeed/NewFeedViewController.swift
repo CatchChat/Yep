eea084c4b190d9c598658d9bae39b2a2c7324a8b
nixzhu <zhuhongxu@gmail.com>
2015-09-29 14:55:39 +0800

    basic UI of NewFeedViewController

--- Yep/ViewControllers/NewFeed/NewFeedViewController.swift
+++ Yep/ViewControllers/NewFeed/NewFeedViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class NewFeedViewController: UIViewController {
 
+    @IBOutlet weak var messageTextView: UITextView!
+
+    @IBOutlet weak var mediaCollectionView: UICollectionView!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = NSLocalizedString("New Feed", comment: "")
+
+        let postButton = UIBarButtonItem(title: NSLocalizedString("Post", comment: ""), style: .Plain, target: self, action: "post:")
+
+        navigationItem.rightBarButtonItem = postButton
+
+        messageTextView.text = "What's up?"
+
+        messageTextView.backgroundColor = UIColor.lightGrayColor()
+        mediaCollectionView.backgroundColor = UIColor.blueColor()
+    }
+
+    // MARK: Actions
+
+    func post(sender: UIBarButtonItem) {
+        println("post")
     }
 }
+
