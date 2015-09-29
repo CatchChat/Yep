273783545c4978db09f33c2301e6c74c330b7e63
nixzhu <zhuhongxu@gmail.com>
2015-09-29 14:37:53 +0800

    add NewFeedViewController; use storyboard reference

---
+++ Yep/ViewControllers/NewFeed/NewFeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewFeedViewController.swift
+//  Yep
+//
+//  Created by nixzhu on 15/9/29.
+//  Copyright © 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class NewFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = NSLocalizedString("New Feed", comment: "")
+    }
+}
