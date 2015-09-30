27c86b1e39915f1a6cd23f72a1bc0d78448960c2
nixzhu <zhuhongxu@gmail.com>
2015-09-30 15:50:42 +0800

    basic UI of mediaCollectionView in NewFeedViewController

--- Yep/ViewControllers/NewFeed/NewFeedViewController.swift
+++ Yep/ViewControllers/NewFeed/NewFeedViewController.swift
@@ -31,6 +31,11 @@ class NewFeedViewController: UIViewController {
         messageTextView.backgroundColor = UIColor.lightGrayColor()
         mediaCollectionView.backgroundColor = UIColor.blueColor()
 
+        mediaCollectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        mediaCollectionView.contentInset.left = 20
+        mediaCollectionView.dataSource = self
+        mediaCollectionView.delegate = self
+
         // try turn on location
 
         let locationResource = PrivateResource.Location(.WhenInUse)
@@ -79,3 +84,28 @@ class NewFeedViewController: UIViewController {
     }
 }
 
+extension NewFeedViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 2
+    }
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath)
+        cell.backgroundColor = UIColor.greenColor()
+        return cell
+    }
+
+    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize {
+
+        return CGSize(width: 80, height: 80)
+    }
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10)
+    }
+}
