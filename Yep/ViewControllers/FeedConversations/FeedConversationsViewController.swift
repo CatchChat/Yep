ef76a76aeb044b29369087cb88f1b0b5d82f7b03
nixzhu <zhuhongxu@gmail.com>
2015-10-12 11:03:08 +0800

    add FeedConversationsViewController

---
+++ Yep/ViewControllers/FeedConversations/FeedConversationsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FeedConversationsViewController.swift
+//  Yep
+//
+//  Created by nixzhu on 15/10/12.
+//  Copyright © 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class FeedConversationsViewController: UIViewController {
+    
+}
