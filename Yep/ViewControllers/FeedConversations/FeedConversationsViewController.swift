28f06ffc18ec9b0d66539d2d3e45bb26f8d9b120
nixzhu <zhuhongxu@gmail.com>
2015-10-12 11:57:35 +0800

    add FeedConversationCell

--- Yep/ViewControllers/FeedConversations/FeedConversationsViewController.swift
+++ Yep/ViewControllers/FeedConversations/FeedConversationsViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class FeedConversationsViewController: UIViewController {
 
+    @IBOutlet weak var feedConversationsTableView: UITableView!
+
+    let feedConversationCellID = "FeedConversationCell"
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Feeds"
+
+        feedConversationsTableView.registerNib(UINib(nibName: feedConversationCellID, bundle: nil), forCellReuseIdentifier: feedConversationCellID)
+        feedConversationsTableView.rowHeight = 80
     }
 }
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+
+extension FeedConversationsViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(feedConversationCellID) as! FeedConversationCell
+        return cell
+    }
+}
+
---
+++ Yep/Views/Cells/FeedConversation/FeedConversationCell.swift
@@ -0,0 +1,28 @@
+//
+//  FeedConversationCell.swift
+//  Yep
+//
+//  Created by nixzhu on 15/10/12.
+//  Copyright © 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class FeedConversationCell: UITableViewCell {
+
+    @IBOutlet weak var mediaView: FeedMediaView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var chatLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
