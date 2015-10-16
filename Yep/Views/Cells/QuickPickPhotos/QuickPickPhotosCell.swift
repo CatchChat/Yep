3e33895e170b4d707a5b1af169da8e5638c919ea
nixzhu <zhuhongxu@gmail.com>
2015-10-16 19:53:05 +0800

    fill QuickPickPhotosCell with photos

--- Yep/Views/Cells/QuickPickPhotos/QuickPickPhotosCell.swift
+++ Yep/Views/Cells/QuickPickPhotos/QuickPickPhotosCell.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import Photos
 
 class QuickPickPhotosCell: UITableViewCell {
 
     @IBOutlet weak var photosCollectionView: UICollectionView!
-    
+
+    var images: PHFetchResult!
+    let imageManager = PHCachingImageManager()
+    var imageCacheController: ImageCacheController!
+
+    var pickedImageSet = Set<PHAsset>()
+    var completion: ((images: [UIImage], imageAssetSet: Set<PHAsset>) -> Void)?
+
+    let photoCellID = "PhotoCell"
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        photosCollectionView.registerNib(UINib(nibName: photoCellID, bundle: nil), forCellWithReuseIdentifier: photoCellID)
+        photosCollectionView.dataSource = self
+        photosCollectionView.delegate = self
+
+        if let layout = photosCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.itemSize = CGSize(width: 70, height: 70)
+            layout.minimumInteritemSpacing = 10
+            layout.sectionInset = UIEdgeInsetsZero
+        }
+
+        images = PHAsset.fetchAssetsWithMediaType(.Image, options: nil)
+        imageCacheController = ImageCacheController(imageManager: imageManager, images: images, preheatSize: 1)
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -22,5 +44,34 @@ class QuickPickPhotosCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
 }
+
+extension QuickPickPhotosCell: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(photoCellID, forIndexPath: indexPath) as! PhotoCell
+        return cell
+    }
+
+    func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+
+        if let cell = cell as? PhotoCell {
+            cell.imageManager = imageManager
+
+            if let imageAsset = images[indexPath.item] as? PHAsset {
+                cell.imageAsset = imageAsset
+                cell.photoPickedImageView.hidden = !pickedImageSet.contains(imageAsset)
+            }
+        }
+    }
+}
+
