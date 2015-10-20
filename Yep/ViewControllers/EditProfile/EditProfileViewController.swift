f8c1014be79256a4a198379801c809b9e3e89317
nixzhu <zhuhongxu@gmail.com>
2015-10-20 18:02:44 +0800

    use Navi in EditProfileViewController

--- Yep/ViewControllers/EditProfile/EditProfileViewController.swift
+++ Yep/ViewControllers/EditProfile/EditProfileViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import RealmSwift
 import TPKeyboardAvoiding
 import Proposer
+import Navi
 
 class EditProfileViewController: UIViewController {
 
@@ -80,19 +81,23 @@ class EditProfileViewController: UIViewController {
 
             let avatarSize = YepConfig.editProfileAvatarSize()
 
-            self.avatarImageView.alpha = 0
-            AvatarCache.sharedInstance.roundAvatarWithAvatarURLString(avatarURLString, withRadius: avatarSize * 0.5) { [weak self] image in
-                dispatch_async(dispatch_get_main_queue()) {
-                    self?.avatarImageView.image = image
+//            self.avatarImageView.alpha = 0
+//            AvatarCache.sharedInstance.roundAvatarWithAvatarURLString(avatarURLString, withRadius: avatarSize * 0.5) { [weak self] image in
+//                dispatch_async(dispatch_get_main_queue()) {
+//                    self?.avatarImageView.image = image
+//
+//                    completion()
+//
+//                    UIView.animateWithDuration(0.2, delay: 0.0, options: .CurveEaseOut, animations: { () -> Void in
+//                        self?.avatarImageView.alpha = 1
+//                    }, completion: { (finished) -> Void in
+//                    })
+//                }
+//            }
 
-                    completion()
-
-                    UIView.animateWithDuration(0.2, delay: 0.0, options: .CurveEaseOut, animations: { () -> Void in
-                        self?.avatarImageView.alpha = 1
-                    }, completion: { (finished) -> Void in
-                    })
-                }
-            }
+            let avatarStyle: AvatarStyle = .RoundedRectangle(size: CGSize(width: avatarSize, height: avatarSize), cornerRadius: avatarSize * 0.5, borderWidth: 0)
+            let plainAvatar = PlainAvatar(avatarURLString: avatarURLString, avatarStyle: avatarStyle)
+            avatarImageView.navi_setAvatar(plainAvatar)
         }
     }
 
