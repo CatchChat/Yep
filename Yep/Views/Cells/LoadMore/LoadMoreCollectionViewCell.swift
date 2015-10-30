02c12fede0d17b3bb23068fd275228cb3eb7b71f
nixzhu <zhuhongxu@gmail.com>
2015-10-30 17:40:31 +0800

    add LoadMoreCollectionViewCell

---
+++ Yep/Views/Cells/LoadMore/LoadMoreCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  LoadMoreCollectionViewCell.swift
+//  Yep
+//
+//  Created by nixzhu on 15/10/30.
+//  Copyright © 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class LoadMoreCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var loadingActivityIndicator: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
