4eacbe6f1c15ea5360ec6865e7211ae40c1d295b
nixzhu <zhuhongxu@gmail.com>
2015-11-24 19:14:38 +0800

    better listener in Profile (fix after change avatar may not update here)

--- Yep/ViewControllers/Profile/ProfileViewController.swift
+++ Yep/ViewControllers/Profile/ProfileViewController.swift
@@ -486,12 +486,9 @@ class ProfileViewController: UIViewController {
 
     lazy var listener: Listener = {
 
-        var myUserID = ""
-        if let profileUser = self.profileUser where profileUser.isMe {
-            myUserID = profileUser.userID
-        }
+        let suffix = NSUUID().UUIDString
 
-        return Listener(nickname: "Profile.Title" + myUserID, introduction: "Profile.introductionText" + myUserID, avatar: "Profile.Avatar" + myUserID)
+        return Listener(nickname: "Profile.Title" + suffix, introduction: "Profile.introductionText" + suffix, avatar: "Profile.Avatar" + suffix)
     }()
 
     // MARK: Life cycle
