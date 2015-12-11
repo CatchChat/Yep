9b0cb0442d1ccc80b8c312872dcd30c1084c3a6f
nixzhu <zhuhongxu@gmail.com>
2015-12-11 11:27:56 +0800

    ActivityIndicator for UIImageView

--- Yep/Extensions/UIImageView+Yep.swift
+++ Yep/Extensions/UIImageView+Yep.swift
@@ -9,8 +9,62 @@
 import UIKit
 import CoreLocation
 
+// MARK: - ActivityIndicator
+
+private var activityIndicatorKey: Void?
+private var showActivityIndicatorWhenLoadingKey: Void?
+
+extension UIImageView {
+
+    private var yep_activityIndicator: UIActivityIndicatorView? {
+        return objc_getAssociatedObject(self, &activityIndicatorKey) as? UIActivityIndicatorView
+    }
+
+    private func yep_setActivityIndicator(activityIndicator: UIActivityIndicatorView?) {
+        objc_setAssociatedObject(self, &activityIndicatorKey, activityIndicator, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+
+    public var yep_showIndicatorWhenLoading: Bool {
+        get {
+            guard let result = objc_getAssociatedObject(self, &showActivityIndicatorWhenLoadingKey) as? NSNumber else {
+                return false
+            }
+
+            return result.boolValue
+        }
+
+        set {
+            if yep_showIndicatorWhenLoading == newValue {
+                return
+
+            } else {
+                if newValue {
+                    let indicatorStyle = UIActivityIndicatorViewStyle.Gray
+                    let indicator = UIActivityIndicatorView(activityIndicatorStyle: indicatorStyle)
+                    indicator.center = CGPointMake(CGRectGetMidX(bounds), CGRectGetMidY(bounds))
+
+                    indicator.autoresizingMask = [.FlexibleLeftMargin, .FlexibleRightMargin, .FlexibleBottomMargin, .FlexibleTopMargin]
+                    indicator.hidden = true
+                    indicator.hidesWhenStopped = true
+
+                    self.addSubview(indicator)
+
+                    yep_setActivityIndicator(indicator)
+
+                } else {
+                    yep_activityIndicator?.removeFromSuperview()
+                    yep_setActivityIndicator(nil)
+                }
+
+                objc_setAssociatedObject(self, &showActivityIndicatorWhenLoadingKey, NSNumber(bool: newValue), .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
+        }
+    }
+}
+
+// MARK: - AttachmentURL
+
 private var attachmentURLKey: Void?
-private var locationxKey: Void?
 
 extension UIImageView {
 
@@ -48,6 +102,13 @@ extension UIImageView {
             println("imageOfAttachment cacheType: \(cacheType)")
         })
     }
+}
+
+// MARK: - Location
+
+private var locationxKey: Void?
+
+extension UIImageView {
 
     private var yep_location: CLLocation? {
         return objc_getAssociatedObject(self, &locationxKey) as? CLLocation
