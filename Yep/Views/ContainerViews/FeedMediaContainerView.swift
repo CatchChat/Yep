3c7d43d65a576fee9de3eb2614b9ef3ab7415dc3
nixzhu <zhuhongxu@gmail.com>
2015-12-16 10:18:50 +0800

    add FeedMediaContainerView

---
+++ Yep/Views/ContainerViews/FeedMediaContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  FeedMediaContainerView.swift
+//  Yep
+//
+//  Created by nixzhu on 15/12/16.
+//  Copyright © 2015年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class FeedMediaContainerView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
