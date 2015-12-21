20e517aae119875f6045e7e9e3e22c2babb2aad9
nixzhu <zhuhongxu@gmail.com>
2015-12-21 09:23:46 +0800

    new STAGING

--- Yep/Services/YepService.swift
+++ Yep/Services/YepService.swift
@@ -11,7 +11,7 @@ import RealmSwift
 import CoreLocation
 
 #if STAGING
-let yepBaseURL = NSURL(string: "https://park-staging.catchchatchina.com")!
+let yepBaseURL = NSURL(string: "https://api.soyep.com")!
 let fayeBaseURL = NSURL(string: "wss://faye-staging.catchchatchina.com/faye")!
 #else
 let yepBaseURL = NSURL(string: "https://park.catchchatchina.com")!
