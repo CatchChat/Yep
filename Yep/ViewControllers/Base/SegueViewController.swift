99ba0da9868045b0ee01e9d1e9dbc34d9c15434b
nixzhu <zhuhongxu@gmail.com>
2016-01-04 09:46:28 +0800

    add SegueViewController override performSegueWithIdentifier to prevent duplicate push

---
+++ Yep/ViewControllers/Base/SegueViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SegueViewController.swift
+//  Yep
+//
+//  Created by nixzhu on 16/1/4.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class SegueViewController: UIViewController {
+
+    override func performSegueWithIdentifier(identifier: String, sender: AnyObject?) {
+
+        if let navigationController = navigationController {
+            guard navigationController.topViewController == self else {
+                return
+            }
+        }
+
+        super.performSegueWithIdentifier(identifier, sender: sender)
+    }
+}
