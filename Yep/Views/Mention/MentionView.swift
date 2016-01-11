58651fc02c572d18b99f24004574a99e63c4ad2d
nixzhu <zhuhongxu@gmail.com>
2016-01-11 13:04:22 +0800

    add MentionView

---
+++ Yep/Views/Mention/MentionView.swift
@@ -0,0 +1,39 @@
+//
+//  MentionView.swift
+//  Yep
+//
+//  Created by nixzhu on 16/1/11.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class MentionView: UIView {
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+
+        makeUI()
+    }
+
+    func makeUI() {
+        addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        let views = [
+            "tableView": tableView,
+        ]
+
+        let constraintsH = NSLayoutConstraint.constraintsWithVisualFormat("H:|[tableView]|", options: [], metrics: nil, views: views)
+        let constraintsV = NSLayoutConstraint.constraintsWithVisualFormat("V:|[tableView]|", options: [], metrics: nil, views: views)
+
+        NSLayoutConstraint.activateConstraints(constraintsH)
+        NSLayoutConstraint.activateConstraints(constraintsV)
+    }
+}
+
