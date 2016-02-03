18f2c04553124864825ce931dbdf3dbd2f259013
nixzhu <zhuhongxu@gmail.com>
2016-02-03 10:18:17 +0800

    remove unused MessageMedia

--- Yep/ViewControllers/Feeds/FeedsViewController.swift
+++ Yep/ViewControllers/Feeds/FeedsViewController.swift
@@ -847,55 +847,6 @@ class FeedsViewController: BaseViewController {
 
             vc.afterCreatedFeedAction = afterCreatedFeedAction
 
-        /*
-        case "showFeedMedia":
-
-            let info = sender as! [String: AnyObject]
-
-            let vc = segue.destinationViewController as! MessageMediaViewController
-
-            if let box = info["attachments"] as? Box<[DiscoveredAttachment]> {
-                let attachments = box.value
-                vc.previewMedias = attachments.map({ PreviewMedia.AttachmentType(attachment: $0) })
-            }
-
-            if let index = info["index"] as? Int {
-                vc.startIndex = index
-            }
-
-            let transitionView = info["transitionView"] as! UIImageView
-
-            let delegate = ConversationMessagePreviewNavigationControllerDelegate()
-            delegate.isFeedMedia = true
-            delegate.snapshot = UIScreen.mainScreen().snapshotViewAfterScreenUpdates(false)
-
-            var frame = transitionView.convertRect(transitionView.frame, toView: view)
-            delegate.frame = frame
-            if let image = transitionView.image {
-                let width = image.size.width
-                let height = image.size.height
-                if width > height {
-                    let newWidth = frame.width * (width / height)
-                    frame.origin.x -= (newWidth - frame.width) / 2
-                    frame.size.width = newWidth
-                } else {
-                    let newHeight = frame.height * (height / width)
-                    frame.origin.y -= (newHeight - frame.height) / 2
-                    frame.size.height = newHeight
-                }
-                delegate.thumbnailImage = image
-            }
-            delegate.thumbnailFrame = frame
-
-            delegate.transitionView = transitionView
-
-            navigationControllerDelegate = delegate
-
-            // 在自定义 push 之前，记录原始的 NavigationControllerDelegate 以便 pop 后恢复
-            originalNavigationControllerDelegate = navigationController!.delegate
-
-            navigationController?.delegate = delegate
-        */
         default:
             break
         }
