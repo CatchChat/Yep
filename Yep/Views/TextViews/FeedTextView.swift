d0f16b9b3f601538b96f11d34f8e43d1dfe9b8b8
nixzhu <zhuhongxu@gmail.com>
2016-02-23 14:20:10 +0800

    mention in FeedTextView

--- Yep/Views/TextViews/FeedTextView.swift
+++ Yep/Views/TextViews/FeedTextView.swift
@@ -47,5 +47,78 @@ class FeedTextView: UITextView {
         
         super.addGestureRecognizer(gestureRecognizer)
     }
+
+    enum DetectionType: String {
+        case Mention
+    }
+
+    var tapMentionAction: ((username: String) -> Void)?
+
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+
+        self.delegate = self
+
+        editable = false
+    }
+
+    override var text: String! {
+        didSet {
+            let attributedString = NSMutableAttributedString(string: text)
+
+            let textRange = NSMakeRange(0, (text as NSString).length)
+
+            attributedString.addAttribute(NSForegroundColorAttributeName, value: textColor!, range: textRange)
+            attributedString.addAttribute(NSFontAttributeName, value: font!, range: textRange)
+
+            // mention link
+
+            let mentionPattern = "[@＠]([A-Za-z0-9_]{4,16})"
+
+            let mentionExpression = try! NSRegularExpression(pattern: mentionPattern, options: NSRegularExpressionOptions())
+
+            mentionExpression.enumerateMatchesInString(text, options: NSMatchingOptions(), range: textRange, usingBlock: { result, flags, stop in
+
+                if let result = result {
+                    let textValue = (self.text as NSString).substringWithRange(result.range)
+
+                    let textAttributes: [String: AnyObject] = [
+                        NSLinkAttributeName: textValue,
+                        ChatTextView.detectionTypeName: DetectionType.Mention.rawValue,
+                    ]
+
+                    attributedString.addAttributes(textAttributes, range: result.range )
+                }
+            })
+            
+            self.attributedText = attributedString
+        }
+    }
 }
 
+extension FeedTextView: UITextViewDelegate {
+
+    func textView(textView: UITextView, shouldInteractWithURL URL: NSURL, inRange characterRange: NSRange) -> Bool {
+
+        guard let detectionTypeName = self.attributedText.attribute(ChatTextView.detectionTypeName, atIndex: characterRange.location, effectiveRange: nil) as? String, detectionType = DetectionType(rawValue: detectionTypeName) else {
+            return true
+        }
+
+        let text = (self.text as NSString).substringWithRange(characterRange)
+
+        self.hangleTapText(text, withDetectionType: detectionType)
+
+        return true
+    }
+
+    private func hangleTapText(text: String, withDetectionType detectionType: DetectionType) {
+
+        println("hangleTapText: \(text), \(detectionType)")
+
+        let username = text.substringFromIndex(text.startIndex.advancedBy(1))
+
+        if !username.isEmpty {
+            tapMentionAction?(username: username)
+        }
+    }
+}
