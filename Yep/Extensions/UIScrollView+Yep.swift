9f8f081314732c1c1a1fbf9829c480f9e6133563
nixzhu <zhuhongxu@gmail.com>
2016-03-03 10:33:21 +0800

    refactor zoomRectWithZoomPoint

--- Yep/Extensions/UIScrollView+Yep.swift
+++ Yep/Extensions/UIScrollView+Yep.swift
@@ -21,7 +21,7 @@ extension UIScrollView {
         setContentOffset(topPoint, animated: true)
     }
 
-    func yep_zoomToPoint(zoomPoint: CGPoint, withScale scale: CGFloat, animated: Bool) {
+    private func yep_zoomRectWithZoomPoint(zoomPoint: CGPoint, scale: CGFloat) -> CGRect {
 
         var scale = min(scale, maximumZoomScale)
         scale = max(scale, minimumZoomScale)
@@ -42,36 +42,25 @@ extension UIScrollView {
             height: destinationRectHeight
         )
 
-        self.zoomToRect(destinationRect, animated: animated)
+        return destinationRect
+    }
+
+    func yep_zoomToPoint(zoomPoint: CGPoint, withScale scale: CGFloat, animated: Bool) {
+
+        let zoomRect = yep_zoomRectWithZoomPoint(zoomPoint, scale: scale)
+        self.zoomToRect(zoomRect, animated: animated)
     }
 
     func yep_zoomToPoint(zoomPoint: CGPoint, withScale scale: CGFloat, duration: NSTimeInterval) {
 
-        var scale = min(scale, maximumZoomScale)
-        scale = max(scale, minimumZoomScale)
-
-        let zoomFactor = 1.0 / self.zoomScale
-
-        let translatedZoomPoint = CGPoint(
-            x: (zoomPoint.x + self.contentOffset.x) * zoomFactor,
-            y: (zoomPoint.y + self.contentOffset.y) * zoomFactor
-        )
-
-        let destinationRectWidth = self.bounds.width / scale
-        let destinationRectHeight = self.bounds.height / scale
-        let destinationRect = CGRect(
-            x: translatedZoomPoint.x - destinationRectWidth * 0.5,
-            y: translatedZoomPoint.y - destinationRectHeight * 0.5,
-            width: destinationRectWidth,
-            height: destinationRectHeight
-        )
+        let zoomRect = yep_zoomRectWithZoomPoint(zoomPoint, scale: scale)
 
         UIView.beginAnimations(nil, context: nil)
         UIView.setAnimationDuration(duration)
         UIView.setAnimationBeginsFromCurrentState(true)
         UIView.setAnimationCurve(.EaseInOut)
 
-        self.zoomToRect(destinationRect, animated: false)
+        self.zoomToRect(zoomRect, animated: false)
 
         UIView.commitAnimations()
     }
