b740bba07bcec6d7fe038a2a0a834822d99c7d4c
nixzhu <zhuhongxu@gmail.com>
2016-03-15 10:29:49 +0800

    use editActions

--- Yep/ViewControllers/FeedConversations/FeedConversationsViewController.swift
+++ Yep/ViewControllers/FeedConversations/FeedConversationsViewController.swift
@@ -177,7 +177,7 @@ extension FeedConversationsViewController: UITableViewDataSource, UITableViewDel
 
         return true
     }
-    
+    /*
     func tableView(tableView: UITableView, titleForDeleteConfirmationButtonForRowAtIndexPath indexPath: NSIndexPath) -> String? {
 
         guard let conversation = feedConversations[safe: indexPath.row] else {
@@ -195,9 +195,94 @@ extension FeedConversationsViewController: UITableViewDataSource, UITableViewDel
 
         return NSLocalizedString("Unsubscribe", comment: "")
     }
+    */
+
+    func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
+
+        guard let conversation = feedConversations[safe: indexPath.row] else {
+            fatalError("Invalid index of feedConversations!")
+        }
+
+        var title: String = NSLocalizedString("Unsubscribe", comment: "")
+        if let feed = conversation.withGroup?.withFeed {
+            if feed.deleted {
+                title = NSLocalizedString("Delete", comment: "")
+            }
+            if let creator = feed.creator where creator.isMe {
+                title = NSLocalizedString("Delete", comment: "")
+            }
+        }
+
+        let deleteAction = UITableViewRowAction(style: .Default, title: title) { [weak self] action, indexPath in
+
+            guard let conversation = self?.feedConversations[safe: indexPath.row] else {
+                tableView.setEditing(false, animated: true)
+                return
+            }
+
+            let doDeleteConversation: () -> Void = {
+
+                dispatch_async(dispatch_get_main_queue()) {
+
+                    guard let realm = conversation.realm else {
+                        return
+                    }
+
+                    realm.beginWrite()
+
+                    deleteConversation(conversation, inRealm: realm)
+
+                    let _ = try? realm.commitWrite()
+
+                    realm.refresh()
+
+                    delay(0.1) {
+                        tableView.setEditing(false, animated: true)
+                        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+
+                        // 延迟一些再发通知，避免影响 tableView 的删除
+                        delay(0.5) {
+                            NSNotificationCenter.defaultCenter().postNotificationName(YepConfig.Notification.changedConversation, object: nil)
+                        }
+                    }
+                }
+            }
+
+            guard let feed = conversation.withGroup?.withFeed, feedCreator = feed.creator else {
+                return
+            }
+
+            let feedID = feed.feedID
+            let feedCreatorID = feedCreator.userID
+
+            // 若是创建者，再询问是否删除 Feed
+
+            if feedCreatorID == YepUserDefaults.userID.value {
+
+                YepAlert.confirmOrCancel(title: NSLocalizedString("Delete", comment: ""), message: NSLocalizedString("Also delete this feed?", comment: ""), confirmTitle: NSLocalizedString("Delete", comment: ""), cancelTitle: NSLocalizedString("Not now", comment: ""), inViewController: self, withConfirmAction: {
+
+                    doDeleteConversation()
+
+                    deleteFeedWithFeedID(feedID, failureHandler: nil, completion: {
+                        println("deleted feed: \(feedID)")
+                    })
+
+                    }, cancelAction: {
+                        doDeleteConversation()
+                })
+                
+            } else {
+                doDeleteConversation()
+            }
+
+            tableView.setEditing(false, animated: true)
+        }
+
+        return [deleteAction]
+    }
 
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-
+        /*
         if editingStyle == .Delete {
             
             guard let conversation = feedConversations[safe: indexPath.row] else {
@@ -260,6 +345,7 @@ extension FeedConversationsViewController: UITableViewDataSource, UITableViewDel
                 doDeleteConversation()
             }
         }
+        */
     }
 }
 
