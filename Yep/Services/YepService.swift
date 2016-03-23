a39c60450c2456d7547aa732a4652ae661bb3b0f
nixzhu <zhuhongxu@gmail.com>
2016-03-23 09:16:05 +0800

    better logic for set conversation.hasUnreadMessages

--- Yep/Services/YepService.swift
+++ Yep/Services/YepService.swift
@@ -1678,7 +1678,7 @@ func officialMessages(completion completion: Int -> Void) {
                                 }
 
                                 // 再设置 conversation，调节 hasUnreadMessages 需要判定 readed
-                                if message.conversation == nil && message.readed == false {
+                                if message.conversation == nil && message.readed == false && message.createdUnixTime > conversation.updatedUnixTime {
                                     conversation.hasUnreadMessages = true
                                 }
                                 message.conversation = conversation
