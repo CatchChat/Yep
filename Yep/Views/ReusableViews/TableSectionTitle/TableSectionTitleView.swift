b53d852162a31d434c27943aa7654146d49657f5
nixzhu <zhuhongxu@gmail.com>
2016-03-23 15:23:13 +0800

    add TableSectionTitleView

---
+++ Yep/Views/ReusableViews/TableSectionTitle/TableSectionTitleView.swift
@@ -0,0 +1,35 @@
+//
+//  TableSectionTitleView.swift
+//  Yep
+//
+//  Created by NIX on 16/3/23.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class TableSectionTitleView: UIView {
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFontOfSize(15)
+        return label
+    }()
+
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+
+        addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        let views = [
+            "titleLable": titleLabel,
+        ]
+
+        let constraintsH = NSLayoutConstraint.constraintsWithVisualFormat("H:|[titleLable]|", options: [], metrics: nil, views: views)
+        let constraintsV = NSLayoutConstraint.constraintsWithVisualFormat("V:|[titleLable]|", options: [], metrics: nil, views: views)
+
+        NSLayoutConstraint.activateConstraints(constraintsH)
+        NSLayoutConstraint.activateConstraints(constraintsV)
+    }
+}
