0a38002f2fc3a117936287fc1a92fa37c2866b8d
nixzhu <zhuhongxu@gmail.com>
2016-03-28 10:42:17 +0800

    add SendingMessagePool

---
+++ Yep/Caches/SendingMessagePool.swift
@@ -0,0 +1,29 @@
+//
+//  SendingMessagePool.swift
+//  Yep
+//
+//  Created by NIX on 16/3/28.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import Foundation
+
+class SendingMessagePool {
+
+    private static var sharedPool = SendingMessagePool()
+
+    private init() {
+    }
+
+    var tempMessageIDSet = Set<String>()
+
+    class func containsMessage(tempMesssageID tempMesssageID: String) -> Bool {
+
+        return sharedPool.tempMessageIDSet.contains(tempMesssageID)
+    }
+
+    class func addMessage(tempMesssageID tempMesssageID: String) {
+
+        sharedPool.tempMessageIDSet.insert(tempMesssageID)
+    }
+}
