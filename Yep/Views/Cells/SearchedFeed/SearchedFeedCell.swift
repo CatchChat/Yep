65db53dd2bcdba8284739e9f4c2621fd8836455c
nixzhu <zhuhongxu@gmail.com>
2016-04-01 15:03:23 +0800

    add SearchedFeedCell

---
+++ Yep/Views/Cells/SearchedFeed/SearchedFeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchedFeedCell.swift
+//  Yep
+//
+//  Created by NIX on 16/4/1.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class SearchedFeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
