148a923e917e1f9e0f17f55b4afb86bfc5f966d6
nixzhu <zhuhongxu@gmail.com>
2016-04-05 14:54:15 +0800

    add SearchSectionTitleCell

---
+++ Yep/Views/Cells/SearchSectionTitle/SearchSectionTitleCell.swift
@@ -0,0 +1,26 @@
+//
+//  SearchSectionTitleCell.swift
+//  Yep
+//
+//  Created by NIX on 16/4/5.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class SearchSectionTitleCell: UITableViewCell {
+
+    @IBOutlet weak var sectionTitleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
