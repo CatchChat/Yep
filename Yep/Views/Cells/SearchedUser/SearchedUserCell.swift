c2c1ded68529b1015df35142ff6e8a765741631c
nixzhu <zhuhongxu@gmail.com>
2016-04-05 09:25:40 +0800

    add SearchedUserCell

---
+++ Yep/Views/Cells/SearchedUser/SearchedUserCell.swift
@@ -0,0 +1,27 @@
+//
+//  SearchedUserCell.swift
+//  Yep
+//
+//  Created by NIX on 16/4/5.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class SearchedUserCell: UITableViewCell {
+
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nicknameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
