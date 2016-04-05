a0c93a699c2e7fe49421301902c1a92462f393ba
nixzhu <zhuhongxu@gmail.com>
2016-04-05 10:51:54 +0800

    add SearchedMessageCell

---
+++ Yep/Views/Cells/SearchedMessage/SearchedMessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  SearchedMessageCell.swift
+//  Yep
+//
+//  Created by NIX on 16/4/5.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class SearchedMessageCell: UITableViewCell {
+
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
