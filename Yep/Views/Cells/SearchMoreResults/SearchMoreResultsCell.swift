29b9741d728f030abceec57658a41fa4a4fac73c
nixzhu <zhuhongxu@gmail.com>
2016-04-06 14:31:04 +0800

    add SearchMoreResultsCell

---
+++ Yep/Views/Cells/SearchMoreResults/SearchMoreResultsCell.swift
@@ -0,0 +1,26 @@
+//
+//  SearchMoreResultsCell.swift
+//  Yep
+//
+//  Created by NIX on 16/4/6.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class SearchMoreResultsCell: UITableViewCell {
+
+    @IBOutlet weak var showMoreLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
