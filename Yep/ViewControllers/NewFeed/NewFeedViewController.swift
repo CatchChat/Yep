c3147e9de10cb02b9f5a482249f59c70d1d8b743
nixzhu <zhuhongxu@gmail.com>
2016-04-11 09:32:47 +0800

    temp fix no NewFeedPreviewViewController

--- Yep/ViewControllers/NewFeed/NewFeedViewController.swift
+++ Yep/ViewControllers/NewFeed/NewFeedViewController.swift
@@ -1113,15 +1113,18 @@ extension NewFeedViewController: UICollectionViewDataSource, UICollectionViewDel
 
         case 1:
 //            let cell = collectionView.cellForItemAtIndexPath(indexPath) as! FeedMediaCell
-            
+
+            /*
             let previewVC = UIStoryboard(name: "NewFeed", bundle: nil).instantiateViewControllerWithIdentifier("NewFeedPreviewViewController") as! NewFeedPreviewViewController
             previewVC.previewImages = mediaImages
             previewVC.startIndex = indexPath.item
             self.navigationController?.pushViewController(previewVC, animated: true)
+             */
 //            mediaImages.removeAtIndex(indexPath.item)
 ////            if !imageAssets.isEmpty {
 ////                imageAssets.removeAtIndex(indexPath.item)
 ////            }
+            break
             
         default:
             break
