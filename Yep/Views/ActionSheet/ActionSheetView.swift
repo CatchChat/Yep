a9681e8b9c671a607187e1e9f8db224dafaacabe
nixzhu <zhuhongxu@gmail.com>
2016-04-13 10:44:21 +0800

    add ActionSheetSubtitleSwitchCell

--- Yep/Views/ActionSheet/ActionSheetView.swift
+++ Yep/Views/ActionSheet/ActionSheetView.swift
@@ -124,6 +124,86 @@ private class ActionSheetSwitchCell: UITableViewCell {
     }
 }
 
+// MARK: - ActionSheetSubtitleSwitchCell
+
+private class ActionSheetSubtitleSwitchCell: UITableViewCell {
+
+    class var reuseIdentifier: String {
+        return "\(self)"
+    }
+
+    var action: (Bool -> Void)?
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        layoutMargins = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+
+        makeUI()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFontOfSize(18, weight: UIFontWeightLight)
+        return label
+    }()
+
+    lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFontOfSize(14, weight: UIFontWeightLight)
+        label.textColor = UIColor.lightGrayColor()
+        return label
+    }()
+
+    lazy var checkedSwitch: UISwitch = {
+        let s = UISwitch()
+        s.addTarget(self, action: #selector(ActionSheetSwitchCell.toggleSwitch(_:)), forControlEvents: .ValueChanged)
+        return s
+    }()
+
+    @objc private func toggleSwitch(sender: UISwitch) {
+        action?(sender.on)
+    }
+
+    func makeUI() {
+        contentView.addSubview(checkedSwitch)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        checkedSwitch.translatesAutoresizingMaskIntoConstraints = false
+
+        let titleStackView = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel])
+
+        titleStackView.axis = .Vertical
+        titleStackView.distribution = .Fill
+        titleStackView.alignment = .Fill
+        titleStackView.spacing = 5
+        titleStackView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(titleStackView)
+
+        do {
+            let centerY = NSLayoutConstraint(item: titleStackView, attribute: .CenterY, relatedBy: .Equal, toItem: contentView, attribute: .CenterY, multiplier: 1, constant: 0)
+            let leading = NSLayoutConstraint(item: titleStackView, attribute: .Leading, relatedBy: .Equal, toItem: contentView, attribute: .Leading, multiplier: 1, constant: 0)
+
+            NSLayoutConstraint.activateConstraints([centerY, leading])
+        }
+
+        do {
+            let centerY = NSLayoutConstraint(item: checkedSwitch, attribute: .CenterY, relatedBy: .Equal, toItem: contentView, attribute: .CenterY, multiplier: 1, constant: 0)
+            let trailing = NSLayoutConstraint(item: checkedSwitch, attribute: .Trailing, relatedBy: .Equal, toItem: contentView, attribute: .Trailing, multiplier: 1, constant: -20)
+
+            NSLayoutConstraint.activateConstraints([centerY, trailing])
+        }
+
+        let gap = NSLayoutConstraint(item: checkedSwitch, attribute: .Leading, relatedBy: .Equal, toItem: titleStackView, attribute: .Trailing, multiplier: 1, constant: 0)
+
+        NSLayoutConstraint.activateConstraints([gap])
+    }
+}
+
 private class ActionSheetCheckCell: UITableViewCell {
 
     class var reuseIdentifier: String {
