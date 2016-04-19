1c89b9c3f98246f05772b74fb7e0025aacf033ac
nixzhu <zhuhongxu@gmail.com>
2016-04-19 11:12:14 +0800

    add SearchedFeedGithubRepoCell

---
+++ Yep/Views/Cells/Feed/SearchedFeed/SearchedFeedGithubRepoCell.swift
@@ -0,0 +1,53 @@
+//
+//  SearchedFeedGithubRepoCell.swift
+//  Yep
+//
+//  Created by NIX on 16/4/19.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class SearchedFeedGithubRepoCell: SearchedFeedBasicCell {
+
+    var tapGithubRepoLinkAction: (NSURL -> Void)?
+
+    lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: SocialAccount.Github.iconName)
+        imageView.frame = CGRect(x: 0, y: 0, width: 18, height: 18)
+        imageView.tintColor = SocialAccount.Github.tintColor
+        return imageView
+    }()
+
+    lazy var githubRepoContainerView: FeedGithubRepoContainerView = {
+        let view = FeedGithubRepoContainerView()
+        view.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        return view
+    }()
+
+    lazy var socialWorkBorderImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "social_work_border")
+        return imageView
+    }()
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        contentView.addSubview(logoImageView)
+        contentView.addSubview(githubRepoContainerView)
+        contentView.addSubview(socialWorkBorderImageView)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
