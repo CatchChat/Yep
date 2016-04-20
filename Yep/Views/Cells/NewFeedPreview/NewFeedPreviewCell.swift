e6eda4ef11e8208f9f35be78a2a5fed8ae47c984
chaiyixiao <bigbigchai@gmail.com>
2016-04-20 14:42:07 +0800

    new feed preview modified / add new avatar for register

--- Yep/Views/Cells/NewFeedPreview/NewFeedPreviewCell.swift
+++ Yep/Views/Cells/NewFeedPreview/NewFeedPreviewCell.swift
@@ -12,11 +12,51 @@ class NewFeedPreviewCell: UICollectionViewCell {
 
     @IBOutlet weak var image: UIImageView!
     
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    let minScale: CGFloat = 1.0
+    let maxScale: CGFloat = 3.0
+    
     override func awakeFromNib() {
         super.awakeFromNib()
 
         image.contentMode = .ScaleAspectFit
-        image.center = self.center
+        image.userInteractionEnabled = true
+        scrollView.contentSize = image.bounds.size
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 3.0
+        
+        let doubleClickGesture = UITapGestureRecognizer(target: self, action: #selector(NewFeedPreviewCell.doubleClick(_:)))
+        doubleClickGesture.numberOfTapsRequired = 2
+        image.addGestureRecognizer(doubleClickGesture)
+    }
+    
+    @objc func doubleClick(sender: UITapGestureRecognizer) {
+        if scrollView.zoomScale != scrollView.maximumZoomScale {
+            scrollView.yep_zoomToPoint(sender.locationInView(self.contentView), withScale: scrollView.maximumZoomScale, animated: true)
+            
+        } else {
+            scrollView.yep_zoomToPoint(sender.locationInView(self.contentView), withScale: scrollView.minimumZoomScale, animated: true)
+        }
+        layoutIfNeeded()
+    }
+    
+    override func prepareForReuse() {
+        scrollView.zoomScale = 1.0
     }
 
+}
+
+extension NewFeedPreviewCell: UIScrollViewDelegate {
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return image
+    }
+}
+
+extension NewFeedPreviewCell: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
 }
