e05e2b15637e1a2127c08df8c8ba250c6d58b05f
nixzhu <zhuhongxu@gmail.com>
2016-04-22 09:51:22 +0800

    add SearchTriggerRepresentation

---
+++ Yep/Protocols/SearchTriggerRepresentation.swift
@@ -0,0 +1,13 @@
+//
+//  SearchTriggerRepresentation.swift
+//  Yep
+//
+//  Created by NIX on 16/4/22.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol SearchTriggerRepresentation {
+
+}
