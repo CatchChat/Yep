81e808ded36b3488bcfbbe13121ba661deb82838
nixzhu <zhuhongxu@gmail.com>
2016-04-22 09:52:09 +0800

    add SearchActionRepresentation

---
+++ Yep/Protocols/SearchActionRepresentation.swift
@@ -0,0 +1,13 @@
+//
+//  SearchActionRepresentation.swift
+//  Yep
+//
+//  Created by NIX on 16/4/22.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol SearchActionRepresentation {
+
+}
