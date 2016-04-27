2a327f64f9db8bd8526a812831d73a51fc5682b4
nixzhu <zhuhongxu@gmail.com>
2016-04-27 15:38:57 +0800

    add ClearTests, testCleanRealmAndCaches

---
+++ YepTests/ClearTests.swift
@@ -0,0 +1,54 @@
+//
+//  ClearTests.swift
+//  Yep
+//
+//  Created by NIX on 16/4/27.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Yep
+import RealmSwift
+
+class ClearTests: XCTestCase {
+
+    func testCleanRealmAndCaches() {
+
+        cleanRealmAndCaches()
+
+        let realm = try! Realm()
+
+        do {
+            let noMessages = realm.objects(Message).isEmpty
+            XCTAssertTrue(noMessages)
+        }
+
+        do {
+            let noUsers = realm.objects(User).isEmpty
+            XCTAssertTrue(noUsers)
+        }
+
+        do {
+            let noGroups = realm.objects(Group).isEmpty
+            XCTAssertTrue(noGroups)
+        }
+
+        do {
+            let noFeeds = realm.objects(Feed).isEmpty
+            XCTAssertTrue(noFeeds)
+        }
+
+        do {
+            let path = NSFileManager.yepMessageCachesURL()?.path
+            let noMessageCacheFiles = try! NSFileManager.defaultManager().contentsOfDirectoryAtPath(path!).isEmpty
+            XCTAssertTrue(noMessageCacheFiles)
+        }
+
+        do {
+            let path = NSFileManager.yepAvatarCachesURL()?.path
+            let noAvatarCacheFiles = try! NSFileManager.defaultManager().contentsOfDirectoryAtPath(path!).isEmpty
+            XCTAssertTrue(noAvatarCacheFiles)
+        }
+    }
+}
+
