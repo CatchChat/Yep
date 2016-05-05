7a4c4fd5164a7fad02256e7a277413a5fa07ca74
nixzhu <zhuhongxu@gmail.com>
2016-05-05 16:39:12 +0800

    add ProfileSocialAccountBlogCell

---
+++ Yep/Views/Cells/ProfileSocialAccount/ProfileSocialAccountBlogCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileSocialAccountBlogCell.swift
+//  Yep
+//
+//  Created by NIX on 16/5/5.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class ProfileSocialAccountBlogCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
