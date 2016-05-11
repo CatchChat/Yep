2765be95b6f4806385d2e26d3685ea5ad1f68427
nixzhu <zhuhongxu@gmail.com>
2016-05-11 10:59:28 +0800

    add FeedFilterCell

---
+++ Yep/Views/Cells/FeedFilter/FeedFilterCell.swift
@@ -0,0 +1,26 @@
+//
+//  FeedFilterCell.swift
+//  Yep
+//
+//  Created by NIX on 16/5/11.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class FeedFilterCell: UITableViewCell {
+
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
