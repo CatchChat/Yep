08da49a28211ca5e05df81fe5f2ee8ebdc10c77a
nixzhu <zhuhongxu@gmail.com>
2016-05-17 16:57:48 +0800

    accès control FayeClient,  init

--- Yep/FayeClient/FayeClient.swift
+++ Yep/FayeClient/FayeClient.swift
@@ -9,7 +9,7 @@
 import Foundation
 import SocketRocket
 
-protocol FayeClientDelegate: class {
+public protocol FayeClientDelegate: class {
 
     func fayeClient(client: FayeClient, didConnectToURL URL: NSURL)
     func fayeClient(client: FayeClient, didDisconnectWithError error: NSError?)
@@ -22,26 +22,66 @@ protocol FayeClientDelegate: class {
     func fayeClient(client: FayeClient, didReceiveMessage messageInfo: [String: AnyObject], fromChannel channel: String)
 }
 
-class FayeClient {
+public class FayeClient {
 
-    var webSocket: SRWebSocket?
-    var serverURL: NSURL!
-    var clientID: String!
+    public private(set) var webSocket: SRWebSocket?
+    public private(set)var serverURL: NSURL!
+    public private(set) var clientID: String!
 
-    var sentMessageCount: Int = 0
+    public private(set) var sentMessageCount: Int = 0
 
-    var subscriptionSet: Set<String> = []
-    var pendingSubscriptionSet: Set<String> = []
-    var openSubscriptionSet: Set<String> = []
+    public private(set) var subscriptionSet: Set<String> = []
+    public private(set) var pendingSubscriptionSet: Set<String> = []
+    public private(set) var openSubscriptionSet: Set<String> = []
 
-    var extensions: [String: AnyObject] = [:]
+    private var subscribedChannels: [String: AnyObject] = [:]
+    private var privateChannels: [String: AnyObject] = [:]
+    private var channelExtensions: [String: AnyObject] = [:]
 
-    var shouldRetryConnection: Bool = true
-    var retryInterval: NSTimeInterval = 1
-    var retryAttempt: Int = 0
-    let maximumRetryAttempts: Int = 5
+    public private(set) var extensions: [String: AnyObject] = [:]
 
-    weak var delegate: FayeClientDelegate?
+    public var shouldRetryConnection: Bool = true
+    public var retryInterval: NSTimeInterval = 1
+    public var retryAttempt: Int = 0
+    public var maximumRetryAttempts: Int = 5
+    private var reconnectTimer: NSTimer?
+
+    public weak var delegate: FayeClientDelegate?
+
+    private var connected: Bool = false
+    private var isConnected: Bool {
+        return connected
+    }
+
+    private var webSocketOpen: Bool = false
+    private var isWebSocketOpen: Bool {
+        return webSocketOpen
+    }
+
+    private var webSocketClosed: Bool = false
+    private var isWebSocketClosed: Bool {
+        return webSocketClosed
+    }
+
+    public init() {
+
+    }
+
+    public convenience init(serverURL: NSURL) {
+        self.init()
+
+        self.serverURL = serverURL
+    }
+
+    public class func client() -> FayeClient {
+
+        return FayeClient()
+    }
+
+    public class func clientWithURL(serverURL: NSURL) -> FayeClient {
+
+        return FayeClient(serverURL: serverURL)
+    }
 
 }
 
