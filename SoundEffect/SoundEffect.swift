8f7b07771bd255e4120a78f27bfffb8dcbab65a7
nixzhu <zhuhongxu@gmail.com>
2016-05-23 17:09:58 +0800

    class SoundEffect

---
+++ SoundEffect/SoundEffect.swift
@@ -0,0 +1,41 @@
+//
+//  SoundEffectHelper.swift
+//  Yep
+//
+//  Created by zhowkevin on 15/9/23.
+//  Copyright © 2015年 Catch Inc. All rights reserved.
+//
+
+import AudioToolbox.AudioServices
+
+final public class SoundEffect: NSObject {
+
+    var soundID: SystemSoundID?
+    
+    public init(soundName: String) {
+        super.init()
+        
+        if let fileURL = NSBundle.mainBundle().URLForResource(soundName, withExtension: "caf") {
+            var theSoundID: SystemSoundID = 0
+            let error = AudioServicesCreateSystemSoundID(fileURL, &theSoundID)
+            if (error == kAudioServicesNoError) {
+                soundID = theSoundID
+            } else {
+                fatalError("SoundEffect init failed!")
+            }
+        }
+    }
+
+    deinit {
+        if let soundID = soundID {
+            AudioServicesDisposeSystemSoundID(soundID)
+        }
+    }
+
+    public func play() {
+        if let soundID = soundID {
+            AudioServicesPlaySystemSound(soundID)
+        }
+    }
+}
+
