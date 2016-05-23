9e22b3b9e75299d99c416f68c7ce7ac787db7e74
nixzhu <zhuhongxu@gmail.com>
2016-05-23 12:59:39 +0800

    add YepKit

---
+++ YepKit/YepKit.h
@@ -0,0 +1,19 @@
+//
+//  YepKit.h
+//  YepKit
+//
+//  Created by NIX on 16/5/23.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YepKit.
+FOUNDATION_EXPORT double YepKitVersionNumber;
+
+//! Project version string for YepKit.
+FOUNDATION_EXPORT const unsigned char YepKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YepKit/PublicHeader.h>
+
+
