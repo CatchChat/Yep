5b729648e02cc709d62a5838c1d2587b3516ba34
nixzhu <zhuhongxu@gmail.com>
2016-05-24 18:01:37 +0800

    clear

--- YepShare/ShareViewController.swift
+++ YepShare/ShareViewController.swift
@@ -109,63 +109,6 @@ class ShareViewController: SLComposeServiceViewController {
 
             self?.extensionContext?.completeRequestReturningItems([], completionHandler: nil)
         }
-
-//        postFeed(message: contentText, URL: urls.first) { [weak self] finish in
-//
-//            print("postFeed URL finish: \(finish)")
-//
-//            self?.extensionContext?.completeRequestReturningItems([], completionHandler: nil)
-//        }
-
-        /*
-        guard let item = extensionContext?.inputItems.first as? NSExtensionItem else {
-
-            extensionContext?.completeRequestReturningItems([], completionHandler: nil)
-            return
-        }
-
-        guard let itemProvider = item.attachments?.first as? NSItemProvider else {
-
-            extensionContext?.completeRequestReturningItems([], completionHandler: nil)
-            return
-        }
-
-        let URLTypeIdentifier = kUTTypeURL as String
-
-        guard itemProvider.hasItemConformingToTypeIdentifier(URLTypeIdentifier) else {
-
-            postFeed(message: contentText, URL: nil) { [weak self] finish in
-
-                print("postFeed onlyText finish: \(finish)")
-
-                self?.extensionContext?.completeRequestReturningItems([], completionHandler: nil)
-            }
-
-            return
-        }
-
-        itemProvider.loadItemForTypeIdentifier(URLTypeIdentifier, options: nil) { [weak self] secureCoding, error in
-
-            guard error == nil else {
-
-                self?.extensionContext?.completeRequestReturningItems([], completionHandler: nil)
-                return
-            }
-
-            guard let URL = secureCoding as? NSURL else {
-
-                self?.extensionContext?.completeRequestReturningItems([], completionHandler: nil)
-                return
-            }
-
-            self?.postFeed(message: self?.contentText, URL: URL) { [weak self] finish in
-
-                print("postFeed URL finish: \(finish)")
-
-                self?.extensionContext?.completeRequestReturningItems([], completionHandler: nil)
-            }
-        }
-         */
     }
 
     override func configurationItems() -> [AnyObject]! {
@@ -217,19 +160,6 @@ class ShareViewController: SLComposeServiceViewController {
 
             doCreateFeed()
 
-//            createFeedWithKind(.Text, message: body, attachments: nil, coordinate: nil, skill: skill, allowComment: true, failureHandler: { reason, errorMessage in
-//                defaultFailureHandler(reason: reason, errorMessage: errorMessage)
-//
-//                dispatch_async(dispatch_get_main_queue()) {
-//                    completion(finish: false)
-//                }
-//
-//            }, completion: { _ in
-//                dispatch_async(dispatch_get_main_queue()) {
-//                    completion(finish: true)
-//                }
-//            })
-
         case .URL(let body, let URL):
 
             let parseOpenGraphGroup = dispatch_group_create()
@@ -274,19 +204,6 @@ class ShareViewController: SLComposeServiceViewController {
                 message = realBody
 
                 doCreateFeed()
-
-//                createFeedWithKind(kind, message: realBody, attachments: attachments, coordinate: nil, skill: self?.skill, allowComment: true, failureHandler: { reason, errorMessage in
-//                    defaultFailureHandler(reason: reason, errorMessage: errorMessage)
-//
-//                    dispatch_async(dispatch_get_main_queue()) {
-//                        completion(finish: false)
-//                    }
-//                    
-//                }, completion: { _ in
-//                    dispatch_async(dispatch_get_main_queue()) {
-//                        completion(finish: true)
-//                    }
-//                })
             }
 
         case .Images(_, let mediaImages):
@@ -365,90 +282,6 @@ class ShareViewController: SLComposeServiceViewController {
             
             uploadImagesQueue.addOperations(uploadAttachmentOperations, waitUntilFinished: false)
             uploadImagesQueue.addOperation(uploadFinishOperation)
-        }
-    }
-
-    private func postFeed(message message: String?, URL: NSURL?, completion: (finish: Bool) -> Void) {
-
-        guard let URL = URL else {
-
-            if let body = message where !body.isEmpty {
-
-                createFeedWithKind(.Text, message: body, attachments: nil, coordinate: nil, skill: skill, allowComment: true, failureHandler: { reason, errorMessage in
-                    defaultFailureHandler(reason: reason, errorMessage: errorMessage)
-
-                    dispatch_async(dispatch_get_main_queue()) {
-                        completion(finish: false)
-                    }
-
-                }, completion: { _ in
-                    dispatch_async(dispatch_get_main_queue()) {
-                        completion(finish: true)
-                    }
-                })
-
-            } else {
-                completion(finish: false)
-            }
-
-            return
-        }
-
-        var kind: FeedKind = .Text
-
-        var attachments: [JSONDictionary]?
-
-        let parseOpenGraphGroup = dispatch_group_create()
-
-        dispatch_group_enter(parseOpenGraphGroup)
-
-        openGraphWithURL(URL, failureHandler: { reason, errorMessage in
-            defaultFailureHandler(reason: reason, errorMessage: errorMessage)
-
-            dispatch_async(dispatch_get_main_queue()) {
-                dispatch_group_leave(parseOpenGraphGroup)
-            }
-
-        }, completion: { openGraph in
-
-            kind = .URL
-
-            let URLInfo = [
-                "url": openGraph.URL.absoluteString,
-                "site_name": (openGraph.siteName ?? "").yepshare_truncatedForFeed,
-                "title": (openGraph.title ?? "").yepshare_truncatedForFeed,
-                "description": (openGraph.description ?? "").yepshare_truncatedForFeed,
-                "image_url": openGraph.previewImageURLString ?? "",
-            ]
-
-            attachments = [URLInfo]
-
-            dispatch_async(dispatch_get_main_queue()) {
-                dispatch_group_leave(parseOpenGraphGroup)
-            }
-        })
-
-        dispatch_group_notify(parseOpenGraphGroup, dispatch_get_main_queue()) { [weak self] in
-
-            let body: String
-            if let message = message where !message.isEmpty {
-                body = message + " " + URL.absoluteString
-            } else {
-                body = URL.absoluteString
-            }
-
-            createFeedWithKind(kind, message: body, attachments: attachments, coordinate: nil, skill: self?.skill, allowComment: true, failureHandler: { reason, errorMessage in
-                defaultFailureHandler(reason: reason, errorMessage: errorMessage)
-
-                dispatch_async(dispatch_get_main_queue()) {
-                    completion(finish: false)
-                }
-                
-            }, completion: { _ in
-                dispatch_async(dispatch_get_main_queue()) {
-                    completion(finish: true)
-                }
-            })
         }
     }
 }
