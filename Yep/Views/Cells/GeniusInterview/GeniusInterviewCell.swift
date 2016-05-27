2c4667138512cf57acd8a059a9d42ae08b26d907
nixzhu <zhuhongxu@gmail.com>
2016-05-27 15:23:52 +0800

    add GeniusInterviewCell

---
+++ Yep/Views/Cells/GeniusInterview/GeniusInterviewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GeniusInterviewCell.swift
+//  Yep
+//
+//  Created by NIX on 16/5/27.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class GeniusInterviewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
