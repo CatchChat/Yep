06dee57c9da09d8c10d897c5716aa0f1e32e9b05
nixzhu <zhuhongxu@gmail.com>
2016-05-27 12:40:34 +0800

    tryMatchSharedFeedWithURL

--- Yep/Views/TextViews/ChatTextView.swift
+++ Yep/Views/TextViews/ChatTextView.swift
@@ -84,6 +84,13 @@ final class ChatTextView: UITextView {
 
 extension ChatTextView: UITextViewDelegate {
 
+    private func tryMatchSharedFeedWithURL(URL: NSURL) -> Bool {
+
+        let matched = URL.yep_matchSharedFeed({ [weak self] feed in self?.tapFeedAction?(feed: feed) })
+
+        return matched
+    }
+
     func textView(textView: UITextView, shouldInteractWithURL URL: NSURL, inRange characterRange: NSRange) -> Bool {
 
         if let detectionTypeName = self.attributedText.attribute(ChatTextView.detectionTypeName, atIndex: characterRange.location, effectiveRange: nil) as? String, detectionType = DetectionType(rawValue: detectionTypeName) {
@@ -93,7 +100,7 @@ extension ChatTextView: UITextViewDelegate {
 
             return false
 
-        } else if URL.yep_matchSharedFeed({ [weak self] feed in self?.tapFeedAction?(feed: feed) }) {
+        } else if tryMatchSharedFeedWithURL(URL) {
             return false
 
         } else {
