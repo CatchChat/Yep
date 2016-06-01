105dc7c45889b2f5075bfb8ae2ca8eeae7678007
nixzhu <zhuhongxu@gmail.com>
2016-06-01 12:30:57 +0800

    add ChatTextCellLayout

---
+++ Yep/Layouts/ChatTextCellLayout.swift
@@ -0,0 +1,12 @@
+//
+//  ChatTextCellLayout.swift
+//  Yep
+//
+//  Created by NIX on 16/6/1.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import Foundation
+
+typealias ChatTextCellLayoutCache = (textContentTextViewFrame: CGRect?, update: ((textContentTextViewFrame: CGRect) -> Void)?)
+
