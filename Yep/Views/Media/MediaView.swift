2439454cc4f0e731cee4d8c023d76fd9e3fe3dd0
nixzhu <zhuhongxu@gmail.com>
2016-06-12 11:08:58 +0800

    better logic for doubleTapToZoom & scrollViewDidZoom

--- Yep/Views/Media/MediaView.swift
+++ Yep/Views/Media/MediaView.swift
@@ -149,7 +149,7 @@ final class MediaView: UIView {
                 isZoomIn = false
                 scrollView.yep_zoomToPoint(zoomPoint, withScale: zoomScale, animated: true)
 
-                scrollView.scrollEnabled = false
+                setNormalScrollViewScrollEnabled()
             }
         }
     }
@@ -271,7 +271,13 @@ extension MediaView: UIScrollViewDelegate {
 
             zoomScaleBeforeZoomIn = scrollView.minimumZoomScale
             isZoomIn = !(scrollView.zoomScale == scrollView.minimumZoomScale)
-            scrollView.scrollEnabled = isZoomIn
+
+            if isZoomIn {
+                scrollView.scrollEnabled = true
+
+            } else {
+                setNormalScrollViewScrollEnabled()
+            }
         }
     }
 }
