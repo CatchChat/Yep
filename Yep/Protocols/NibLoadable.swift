58952590e78cb28cb338f8788e1f204600ea1074
nixzhu <zhuhongxu@gmail.com>
2016-06-14 11:40:40 +0800

    add NibLoadable protocol

---
+++ Yep/Protocols/NibLoadable.swift
@@ -0,0 +1,29 @@
+//
+//  NibLoadable.swift
+//  Yep
+//
+//  Created by NIX on 16/6/14.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+protocol NibLoadable {
+
+    static var nibName: String { get }
+}
+
+extension UITableViewCell: NibLoadable {
+
+    static var nibName: String {
+        return "\(self)"
+    }
+}
+
+extension UICollectionViewCell: NibLoadable {
+
+    static var nibName: String {
+        return "\(self)"
+    }
+}
+
