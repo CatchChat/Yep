bffcc74ae871c6f6c21759a1226101a0b2df9e76
nixzhu <zhuhongxu@gmail.com>
2016-06-16 16:05:51 +0800

    updateZoomScaleWithImage & centerContent

--- YepPreview/ScalingImageView.swift
+++ YepPreview/ScalingImageView.swift
@@ -58,6 +58,46 @@ class ScalingImageView: UIScrollView {
         imageView.frame = CGRect(origin: CGPointZero, size: image.size)
 
         contentSize = image.size
+
+        updateZoomScaleWithImage(image)
+        centerContent()
+    }
+
+    private func updateZoomScaleWithImage(image: UIImage) {
+
+        let scrollViewFrame = bounds
+        let imageSize = image.size
+
+        let widthScale = scrollViewFrame.width / imageSize.width
+        let heightScale = scrollViewFrame.height / imageSize.height
+
+        let minScale = min(widthScale, heightScale)
+        minimumZoomScale = minScale
+        maximumZoomScale = max(minScale, maximumZoomScale)
+        zoomScale = minimumZoomScale
+
+        panGestureRecognizer.enabled = false
+    }
+
+    private func centerContent() {
+
+        var horizontalInset: CGFloat = 0
+        var verticalInset: CGFloat = 0
+
+        if contentSize.width < bounds.width {
+            horizontalInset = (bounds.width - contentSize.width) * 0.5
+        }
+
+        if contentSize.height < bounds.height {
+            verticalInset = (bounds.height - contentSize.height) * 0.5
+        }
+
+        if let scale = window?.screen.scale where scale < 2 {
+            horizontalInset = floor(horizontalInset)
+            verticalInset = floor(verticalInset)
+        }
+
+        contentInset = UIEdgeInsets(top: verticalInset, left: horizontalInset, bottom: verticalInset, right: horizontalInset)
     }
 
 }
