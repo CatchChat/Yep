f85d052e88e8bab4e2705f50e25adcf054b3fcd1
nixzhu <zhuhongxu@gmail.com>
2016-06-17 16:06:17 +0800

    add PhotoTransitionAnimator

---
+++ YepPreview/Transiton/PhotoTransitionAnimator.swift
@@ -0,0 +1,44 @@
+//
+//  PhotoTransitionAnimator.swift
+//  Yep
+//
+//  Created by NIX on 16/6/17.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTransitionAnimator: NSObject {
+
+    var startingView: UIView?
+    var endingView: UIView?
+
+    var startingViewForAnimation: UIView?
+    var endingViewForAnimation: UIView?
+
+    var isDismissing: Bool = false
+
+    var animationDurationWithZooming: NSTimeInterval = 0.5
+    var animationDurationWithoutZooming: NSTimeInterval = 0.3
+
+    var animationDurationFadeRatio: CGFloat = 4
+    var animationDurationEndingViewFadeInRatio: CGFloat = 0.1
+    var animationDurationStartingViewFadeOutRatio: CGFloat = 0.05
+
+    var zoomingAnimationSpringDamping: CGFloat = 0.9
+
+
+}
+
+extension PhotoTransitionAnimator: UIViewControllerAnimatedTransitioning {
+
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+
+        return animationDurationWithoutZooming
+    }
+
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+    }
+}
+
---
+++ YepPreview/Transiton/PhotoTransitonController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoTransitonController.swift
+//  Yep
+//
+//  Created by NIX on 16/6/17.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTransitonController: NSObject, UIViewControllerTransitioningDelegate {
+
+}
