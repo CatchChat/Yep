ff47f952fa03961a7eba159fe1835e0dd83fddca
nixzhu <zhuhongxu@gmail.com>
2016-06-17 09:25:37 +0800

    add PhotoViewController

---
+++ YepPreview/PhotoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PhotoViewController.swift
+//  Yep
+//
+//  Created by NIX on 16/6/17.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    let photo: Photo
+
+    private var scalingImageView: ScalingImageView?
+
+    deinit {
+        scalingImageView?.delegate = nil
+
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+
+    init(photo: Photo) {
+        self.photo = photo
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
