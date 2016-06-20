1d2c0536daf89d897258080b81f0bd9c9a56775f
nixzhu <zhuhongxu@gmail.com>
2016-06-20 11:41:24 +0800

    add PreviewAttachmentPhoto

---
+++ Yep/PreviewPhotos/PreviewAttachmentPhoto.swift
@@ -0,0 +1,35 @@
+//
+//  PreviewAttachmentPhoto.swift
+//  Yep
+//
+//  Created by NIX on 16/6/20.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import Foundation
+import YepKit
+import YepPreview
+
+class PreviewAttachmentPhoto: Photo {
+
+    var attachment: DiscoveredAttachment
+    var image: UIImage?
+
+    init(attachment: DiscoveredAttachment) {
+        self.attachment = attachment
+
+        ImageCache.sharedInstance.imageOfAttachment(attachment, withMinSideLength: nil) { (url, image, cacheType) in
+            self.image = image
+        }
+    }
+
+    var imageType: ImageType {
+
+        if let image = image {
+            return .image(image)
+        } else {
+            return .imageURL(NSURL(string: attachment.URLString)!)
+        }
+    }
+}
+
