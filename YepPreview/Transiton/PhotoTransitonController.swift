a362e1cc27b95245152cdf5b80b7df6551f13603
nixzhu <zhuhongxu@gmail.com>
2016-06-20 10:04:57 +0800

    PhotoTransitonController

--- YepPreview/Transiton/PhotoTransitonController.swift
+++ YepPreview/Transiton/PhotoTransitonController.swift
@@ -10,4 +10,27 @@ import UIKit
 
 class PhotoTransitonController: NSObject, UIViewControllerTransitioningDelegate {
 
+    lazy var animator = PhotoTransitionAnimator()
+    lazy var interactionController = PhotoDismissalInteractionController()
+
+    var startingView: UIView? {
+        return animator.startingView
+    }
+
+    func setStartingView(view: UIView) {
+        animator.startingView = view
+    }
+
+    var endingView: UIView? {
+        return animator.endingView
+    }
+
+    func setEndingView(view: UIView) {
+        animator.endingView = view
+    }
+
+    func didPanWithPanGestureRecognizer(pan: UIPanGestureRecognizer, viewToPan: UIView, anchorPoint: CGPoint) {
+
+        interactionController.didPanWithPanGestureRecognizer(pan, viewToPan: viewToPan, anchorPoint: anchorPoint)
+    }
 }
