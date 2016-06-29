48f5a2579893bd85d1f353f2d6ea2312f2c9a9be
nixzhu <zhuhongxu@gmail.com>
2016-06-29 16:33:46 +0800

    add GeniusInterviewViewController

---
+++ Yep/ViewControllers/GeniusInterview/GeniusInterviewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GeniusInterviewViewController.swift
+//  Yep
+//
+//  Created by NIX on 16/6/29.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+
+class GeniusInterviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
