c4a7901dc27b038bf146ac61b887429a9a3beac3
nixzhu <zhuhongxu@gmail.com>
2016-07-07 10:30:00 +0800

    LeftBubbleNode

--- Yep/ViewControllers/Chat/CellNodes/ChatLeftTextCellNode.swift
+++ Yep/ViewControllers/Chat/CellNodes/ChatLeftTextCellNode.swift
@@ -10,6 +10,39 @@ import UIKit
 import YepKit
 import AsyncDisplayKit
 
+class LeftBubbleNode: ASDisplayNode {
+
+    override class func drawRect(bounds: CGRect, withParameters parameters: NSObjectProtocol!, isCancelled isCancelledBlock: asdisplaynode_iscancelled_block_t, isRasterizing: Bool) {
+
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSaveGState(context)
+
+        UIColor.leftBubbleTintColor().setFill()
+
+        do {
+            let path = UIBezierPath()
+            let arrowPoint = CGPoint(x: 0, y: 20)
+            let topPoint = CGPoint(x: 10, y: 10)
+            let topControlPoint = CGPoint(x: 7, y: 20)
+            let bottomPoint = CGPoint(x: 10, y: 30)
+            let bottomControlPoint = CGPoint(x: 7, y: 20)
+            path.moveToPoint(arrowPoint)
+            path.addQuadCurveToPoint(topPoint, controlPoint: topControlPoint)
+            path.addLineToPoint(bottomPoint)
+            path.addQuadCurveToPoint(arrowPoint, controlPoint: bottomControlPoint)
+            path.fill()
+        }
+
+        do {
+            let rect = CGRect(x: 8, y: 0, width: bounds.width - 8, height: bounds.height)
+            let path = UIBezierPath(roundedRect: rect, cornerRadius: 15)
+            path.fill()
+        }
+
+        CGContextRestoreGState(context)
+    }
+}
+
 class ChatLeftTextCellNode: ChatLeftBaseCellNode {
 
     static let textAttributes = [
@@ -17,12 +50,12 @@ class ChatLeftTextCellNode: ChatLeftBaseCellNode {
         NSFontAttributeName: UIFont.chatTextFont(),
     ]
 
-    lazy var bubbleNode: ASDisplayNode = {
-        let node = ASDisplayNode()
-        node.layerBacked = true
-        node.clipsToBounds = true
-        node.cornerRadius = 20
-        node.backgroundColor = UIColor.leftBubbleTintColor()
+    lazy var bubbleNode: LeftBubbleNode = {
+        let node = LeftBubbleNode()
+//        node.layerBacked = true
+//        node.clipsToBounds = true
+//        node.cornerRadius = 20
+//        node.backgroundColor = UIColor.leftBubbleTintColor()
         return node
     }()
 
@@ -32,7 +65,7 @@ class ChatLeftTextCellNode: ChatLeftBaseCellNode {
         super.init()
 
         addSubnode(bubbleNode)
-        addSubnode(textNode)
+        //addSubnode(textNode)
         textNode.backgroundColor = UIColor.greenColor()
     }
 
