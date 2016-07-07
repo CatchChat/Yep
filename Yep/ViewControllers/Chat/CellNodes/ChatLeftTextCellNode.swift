7df6736829d0d4bb77b69a6cc95ea129af32c77b
nixzhu <zhuhongxu@gmail.com>
2016-07-07 11:10:59 +0800

    use tailImageNode

--- Yep/ViewControllers/Chat/CellNodes/ChatLeftTextCellNode.swift
+++ Yep/ViewControllers/Chat/CellNodes/ChatLeftTextCellNode.swift
@@ -10,39 +10,6 @@ import UIKit
 import YepKit
 import AsyncDisplayKit
 
-class LeftBubbleNode: ASDisplayNode {
-
-    override class func drawRect(bounds: CGRect, withParameters parameters: NSObjectProtocol!, isCancelled isCancelledBlock: asdisplaynode_iscancelled_block_t, isRasterizing: Bool) {
-
-        let context = UIGraphicsGetCurrentContext()
-        CGContextSaveGState(context)
-
-        UIColor.leftBubbleTintColor().setFill()
-
-        do {
-            let path = UIBezierPath()
-            let arrowPoint = CGPoint(x: 0, y: 20)
-            let topPoint = CGPoint(x: 11, y: 10)
-            let topControlPoint = CGPoint(x: 7, y: 20)
-            let bottomPoint = CGPoint(x: 11, y: 30)
-            let bottomControlPoint = CGPoint(x: 7, y: 20)
-            path.moveToPoint(arrowPoint)
-            path.addQuadCurveToPoint(topPoint, controlPoint: topControlPoint)
-            path.addLineToPoint(bottomPoint)
-            path.addQuadCurveToPoint(arrowPoint, controlPoint: bottomControlPoint)
-            path.fill()
-        }
-
-        do {
-            let rect = CGRect(x: 8, y: 0, width: bounds.width - 8, height: bounds.height)
-            let path = UIBezierPath(roundedRect: rect, cornerRadius: 15)
-            path.fill()
-        }
-
-        CGContextRestoreGState(context)
-    }
-}
-
 class ChatLeftTextCellNode: ChatLeftBaseCellNode {
 
     static let textAttributes = [
@@ -50,8 +17,18 @@ class ChatLeftTextCellNode: ChatLeftBaseCellNode {
         NSFontAttributeName: UIFont.chatTextFont(),
     ]
 
-    lazy var bubbleNode: LeftBubbleNode = {
-        let node = LeftBubbleNode()
+    lazy var tailImageNode: ASImageNode = {
+        let node = ASImageNode()
+        node.image = UIImage(named: "bubble_left_tail")?.imageWithRenderingMode(.AlwaysOriginal)
+        return node
+    }()
+
+    lazy var bubbleNode: ASDisplayNode = {
+        let node = ASDisplayNode()
+        node.layerBacked = true
+        node.clipsToBounds = true
+        node.cornerRadius = 20
+        node.backgroundColor = UIColor.leftBubbleTintColor()
         return node
     }()
 
@@ -60,9 +37,9 @@ class ChatLeftTextCellNode: ChatLeftBaseCellNode {
     override init() {
         super.init()
 
+        addSubnode(tailImageNode)
         addSubnode(bubbleNode)
         addSubnode(textNode)
-        textNode.backgroundColor = UIColor.greenColor()
     }
 
     func configure(withMessage message: Message) {
@@ -77,10 +54,10 @@ class ChatLeftTextCellNode: ChatLeftBaseCellNode {
 
     override func calculateSizeThatFits(constrainedSize: CGSize) -> CGSize {
 
-        let textMaxWidth = constrainedSize.width - (15 + ChatBaseCellNode.avatarSize.width + (5 + 8 + 10) + 15)
+        let textMaxWidth = constrainedSize.width - (15 + ChatBaseCellNode.avatarSize.width + (5 + 7 + 10) + (10 + 15))
         textNode.measure(CGSize(width: textMaxWidth, height: CGFloat.max))
 
-        let height = max(textNode.calculatedSize.height + (10 + 10), ChatBaseCellNode.avatarSize.height)
+        let height = max(textNode.calculatedSize.height + (7 + 7), ChatBaseCellNode.avatarSize.height)
 
         return CGSize(width: constrainedSize.width, height: height)
     }
@@ -88,12 +65,14 @@ class ChatLeftTextCellNode: ChatLeftBaseCellNode {
     override func layout() {
         super.layout()
 
-        let x = 15 + ChatBaseCellNode.avatarSize.width + (5 + 8 + 10)
+        let x = 15 + ChatBaseCellNode.avatarSize.width + (5 + 7 + 10)
         let y = (calculatedSize.height - textNode.calculatedSize.height) / 2
         let origin = CGPoint(x: x, y: y)
         textNode.frame = CGRect(origin: origin, size: textNode.calculatedSize)
 
-        bubbleNode.frame = CGRect(x: x - (8 + 10), y: 0, width: textNode.calculatedSize.width + ((8 + 10) + 10), height: calculatedSize.height)
+        bubbleNode.frame = CGRect(x: x - 10, y: 0, width: textNode.calculatedSize.width + (10 + 10), height: calculatedSize.height)
+
+        tailImageNode.frame = CGRect(x: x - (7 + 10), y: 20 - (14 / 2), width: 13, height: 14)
     }
 }
 
