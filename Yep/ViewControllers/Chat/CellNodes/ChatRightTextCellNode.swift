d2fbc851e6995e4c3892c96bdb4c16f9e33ca349
nixzhu <zhuhongxu@gmail.com>
2016-07-07 11:37:36 +0800

    bubble in ChatRightTextCellNode

--- Yep/ViewControllers/Chat/CellNodes/ChatRightTextCellNode.swift
+++ Yep/ViewControllers/Chat/CellNodes/ChatRightTextCellNode.swift
@@ -13,17 +13,33 @@ import AsyncDisplayKit
 class ChatRightTextCellNode: ChatRightBaseCellNode {
 
     static let textAttributes = [
-        NSForegroundColorAttributeName: UIColor.blackColor(),
+        NSForegroundColorAttributeName: UIColor.whiteColor(),
         NSFontAttributeName: UIFont.chatTextFont(),
     ]
+
+    lazy var tailImageNode: ASImageNode = {
+        let node = ASImageNode()
+        node.image = UIImage(named: "bubble_right_tail")?.imageWithRenderingMode(.AlwaysOriginal)
+        return node
+    }()
+
+    lazy var bubbleNode: ASDisplayNode = {
+        let node = ASDisplayNode()
+        node.layerBacked = true
+        node.clipsToBounds = true
+        node.cornerRadius = 20
+        node.backgroundColor = UIColor.rightBubbleTintColor()
+        return node
+    }()
 
     lazy var textNode = ASTextNode()
 
     override init() {
         super.init()
 
+        addSubnode(tailImageNode)
+        addSubnode(bubbleNode)
         addSubnode(textNode)
-        textNode.backgroundColor = UIColor.greenColor()
     }
 
     func configure(withMessage message: Message) {
@@ -38,10 +54,10 @@ class ChatRightTextCellNode: ChatRightBaseCellNode {
 
     override func calculateSizeThatFits(constrainedSize: CGSize) -> CGSize {
 
-        let textMaxWidth = constrainedSize.width - (15 + ChatBaseCellNode.avatarSize.width + 5 + 15)
+        let textMaxWidth = constrainedSize.width - ((15 + 10) + ChatBaseCellNode.avatarSize.width + (5 + 7 + 10) + 15)
         textNode.measure(CGSize(width: textMaxWidth, height: CGFloat.max))
 
-        let height = max(textNode.calculatedSize.height, ChatBaseCellNode.avatarSize.height)
+        let height = max(textNode.calculatedSize.height + (7 + 7), ChatBaseCellNode.avatarSize.height)
 
         return CGSize(width: constrainedSize.width, height: height)
     }
@@ -49,10 +65,20 @@ class ChatRightTextCellNode: ChatRightBaseCellNode {
     override func layout() {
         super.layout()
 
-        let x = calculatedSize.width - (textNode.calculatedSize.width + 5 + ChatBaseCellNode.avatarSize.width + 15)
-        let y: CGFloat = 0
-        let origin = CGPoint(x: x, y: y)
+        let x = calculatedSize.width - (textNode.calculatedSize.width + (10 + 7 + 5) + ChatBaseCellNode.avatarSize.width + 15)
+        let y = (calculatedSize.height - textNode.calculatedSize.height) / 2
+        let bubbleNodeMinWidth: CGFloat = 40
+        let textNodeMinWidth = bubbleNodeMinWidth - (10 + 10)
+        let offsetX = min(textNodeMinWidth, textNode.calculatedSize.width) < textNodeMinWidth ? ((textNodeMinWidth - textNode.calculatedSize.width) / 2) : 0
+        let origin = CGPoint(x: x + offsetX, y: y)
         textNode.frame = CGRect(origin: origin, size: textNode.calculatedSize)
+
+        bubbleNode.frame = CGRect(x: x - 10, y: 0, width: max(textNodeMinWidth, textNode.calculatedSize.width) + (10 + 10), height: calculatedSize.height)
+
+        do {
+            let x = calculatedSize.width - ((13 + 5) + ChatBaseCellNode.avatarSize.width + 15)
+            tailImageNode.frame = CGRect(x: x, y: 20 - (14 / 2), width: 13, height: 14)
+        }
     }
 }
 
