da22d7039624db21ca85c82a1f4774392b140f9e
nixzhu <zhuhongxu@gmail.com>
2016-07-12 15:57:09 +0800

    enum Interview

--- Yep/ViewControllers/GeniusInterview/GeniusInterviewViewController.swift
+++ Yep/ViewControllers/GeniusInterview/GeniusInterviewViewController.swift
@@ -13,7 +13,29 @@ import RealmSwift
 
 class GeniusInterviewViewController: UIViewController {
 
-    var geniusInterview: GeniusInterview!
+    enum Interview {
+        case geniusInterview(GeniusInterview)
+        case geniusInterviewBanner(GeniusInterviewBanner)
+
+        var user: DiscoveredUser {
+            switch self {
+            case .geniusInterview(let geniusInterview):
+                return geniusInterview.user
+            case .geniusInterviewBanner(let geniusInterviewBanner):
+                return geniusInterviewBanner.user
+            }
+        }
+
+        var linkURL: NSURL {
+            switch self {
+            case .geniusInterview(let geniusInterview):
+                return geniusInterview.url
+            case .geniusInterviewBanner(let geniusInterviewBanner):
+                return geniusInterviewBanner.linkURL
+            }
+        }
+    }
+    var interview: Interview!
 
     private let actionViewHeight: CGFloat = 50
 
@@ -43,7 +65,7 @@ class GeniusInterviewViewController: UIViewController {
         let view = GeniusInterviewActionView()
 
         view.tapAvatarAction = { [weak self] in
-            guard let user = self?.geniusInterview.user else {
+            guard let user = self?.interview.user else {
                 return
             }
 
@@ -53,7 +75,7 @@ class GeniusInterviewViewController: UIViewController {
         }
 
         view.sayHiAction = { [weak self] in
-            guard let user = self?.geniusInterview.user else {
+            guard let user = self?.interview.user else {
                 return
             }
 
@@ -76,7 +98,7 @@ class GeniusInterviewViewController: UIViewController {
         }
 
         view.shareAction = { [weak self] in
-            guard let url = self?.geniusInterview.url else {
+            guard let url = self?.interview.linkURL else {
                 return
             }
 
@@ -130,14 +152,14 @@ class GeniusInterviewViewController: UIViewController {
         }
 
         do {
-            let request = NSURLRequest(URL: geniusInterview.url)
+            let request = NSURLRequest(URL: interview.linkURL)
             webView.loadRequest(request)
 
             indicatorView.startAnimating()
         }
 
         do {
-            let avatar = PlainAvatar(avatarURLString: geniusInterview.user.avatarURLString, avatarStyle: miniAvatarStyle)
+            let avatar = PlainAvatar(avatarURLString: interview.user.avatarURLString, avatarStyle: miniAvatarStyle)
             actionView.avatarImageView.navi_setAvatar(avatar, withFadeTransitionDuration: avatarFadeTransitionDuration)
         }
     }
