c64a578995431c0dff7a0b42d6ce40b37a6e41ad
nixzhu <zhuhongxu@gmail.com>
2016-07-13 15:24:27 +0800

    move send(videoWithVideoURL:)

--- Yep/ViewControllers/Chat/ChatViewController+UIImagePickerControllerDelegate.swift
+++ Yep/ViewControllers/Chat/ChatViewController+UIImagePickerControllerDelegate.swift
@@ -36,7 +36,7 @@ extension ChatViewController: UIImagePickerControllerDelegate, UINavigationContr
 
                 if let videoURL = info[UIImagePickerControllerMediaURL] as? NSURL {
                     println("videoURL \(videoURL)")
-                    sendVideoWithVideoURL(videoURL)
+                    send(videoWithVideoURL: videoURL)
                 }
 
             default:
@@ -47,129 +47,5 @@ extension ChatViewController: UIImagePickerControllerDelegate, UINavigationContr
         dismissViewControllerAnimated(true, completion: nil)
     }
 
-    private func sendVideoWithVideoURL(videoURL: NSURL) {
-
-        // Prepare meta data
-
-        var metaData: String? = nil
-
-        var thumbnailData: NSData?
-
-        if let image = thumbnailImageOfVideoInVideoURL(videoURL) {
-
-            let imageWidth = image.size.width
-            let imageHeight = image.size.height
-
-            let thumbnailWidth: CGFloat
-            let thumbnailHeight: CGFloat
-
-            if imageWidth > imageHeight {
-                thumbnailWidth = min(imageWidth, Config.MetaData.thumbnailMaxSize)
-                thumbnailHeight = imageHeight * (thumbnailWidth / imageWidth)
-            } else {
-                thumbnailHeight = min(imageHeight, Config.MetaData.thumbnailMaxSize)
-                thumbnailWidth = imageWidth * (thumbnailHeight / imageHeight)
-            }
-
-            let videoMetaDataInfo: [String: AnyObject]
-
-            let thumbnailSize = CGSize(width: thumbnailWidth, height: thumbnailHeight)
-
-            if let thumbnail = image.resizeToSize(thumbnailSize, withInterpolationQuality: CGInterpolationQuality.Low) {
-                let blurredThumbnail = thumbnail.blurredImageWithRadius(5, iterations: 7, tintColor: UIColor.clearColor())
-
-                let data = UIImageJPEGRepresentation(blurredThumbnail, 0.7)!
-
-                let string = data.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
-
-                println("video blurredThumbnail string length: \(string.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))\n")
-
-                videoMetaDataInfo = [
-                    Config.MetaData.videoWidth: imageWidth,
-                    Config.MetaData.videoHeight: imageHeight,
-                    Config.MetaData.blurredThumbnailString: string,
-                ]
-
-            } else {
-                videoMetaDataInfo = [
-                    Config.MetaData.videoWidth: imageWidth,
-                    Config.MetaData.videoHeight: imageHeight,
-                ]
-            }
-
-            if let videoMetaData = try? NSJSONSerialization.dataWithJSONObject(videoMetaDataInfo, options: []) {
-                let videoMetaDataString = NSString(data: videoMetaData, encoding: NSUTF8StringEncoding) as? String
-                metaData = videoMetaDataString
-            }
-
-            thumbnailData = UIImageJPEGRepresentation(image, YepConfig.messageImageCompressionQuality())
-        }
-
-        let messageVideoName = NSUUID().UUIDString
-
-        let afterCreatedMessageAction = { [weak self] (message: Message) in
-
-            SafeDispatch.async {
-
-                if let videoData = NSData(contentsOfURL: videoURL) {
-
-                    if let _ = NSFileManager.saveMessageVideoData(videoData, withName: messageVideoName) {
-                        if let realm = message.realm {
-                            let _ = try? realm.write {
-
-                                if let thumbnailData = thumbnailData {
-                                    if let _ = NSFileManager.saveMessageImageData(thumbnailData, withName: messageVideoName) {
-                                        message.localThumbnailName = messageVideoName
-                                    }
-                                }
-
-                                message.localAttachmentName = messageVideoName
-
-                                message.mediaType = MessageMediaType.Video.rawValue
-                                if let metaDataString = metaData {
-                                    message.mediaMetaData = mediaMetaDataFromString(metaDataString, inRealm: realm)
-                                }
-                            }
-                        }
-                    }
-
-                    self?.update(withMessageIDs: nil, messageAge: .New, scrollToBottom: true, success: { _ in
-                    })
-                }
-            }
-        }
-
-        if let withFriend = conversation.withFriend {
-
-            sendVideoInFilePath(videoURL.path!, orFileData: nil, metaData: metaData, toRecipient: withFriend.userID, recipientType: "User", afterCreatedMessage: afterCreatedMessageAction, failureHandler: { [weak self] reason, errorMessage in
-                defaultFailureHandler(reason: reason, errorMessage: errorMessage)
-
-                /*
-                self?.promptSendMessageFailed(
-                    reason: reason,
-                    errorMessage: errorMessage,
-                    reserveErrorMessage: NSLocalizedString("Failed to send video!\nTry tap on message to resend.", comment: "")
-                )*/
-                
-            }, completion: { [weak self] success in
-                println("send video to friend: \(success)")
-                
-                //self?.showFriendRequestViewIfNeed()
-            })
-            
-        } else if let withGroup = conversation.withGroup {
-            
-            sendVideoInFilePath(videoURL.path!, orFileData: nil, metaData: metaData, toRecipient: withGroup.groupID, recipientType: "Circle", afterCreatedMessage: afterCreatedMessageAction, failureHandler: { [weak self] reason, errorMessage in
-                defaultFailureHandler(reason: reason, errorMessage: errorMessage)
-                
-                YepAlert.alertSorry(message: NSLocalizedString("Failed to send video!\nTry tap on message to resend.", comment: ""), inViewController: self)
-                
-            }, completion: { [weak self] success in
-                println("send video to group: \(success)")
-                
-                //self?.updateGroupToIncludeMe()
-            })
-        }
-    }
 }
 
