2af57976aa529137871811c190d45656f672edc2
nixzhu <zhuhongxu@gmail.com>
2016-07-18 17:48:57 +0800

    FreeTimeJob.setup

--- Yep/RunLoop/FreeTimeJob.swift
+++ Yep/RunLoop/FreeTimeJob.swift
@@ -10,6 +10,31 @@ import Foundation
 
 class FreeTimeJob: NSObject {
 
+    static var set = NSMutableSet()
+
+    private class func setup() {
+
+        var onceToken : dispatch_once_t = 0
+        dispatch_once(&onceToken) {
+            let runLoop = CFRunLoopGetMain()
+            let observer: CFRunLoopObserver = CFRunLoopObserverCreateWithHandler(kCFAllocatorDefault, CFRunLoopActivity.BeforeWaiting.rawValue | CFRunLoopActivity.Exit.rawValue, true, 0xFFFFFF) { (observer, activity) in
+                guard set.count != 0 else {
+                    return
+                }
+
+                let currentSet = set
+                set = NSMutableSet()
+
+                currentSet.enumerateObjectsUsingBlock({ (object, stop) in
+                    if let job = object as? FreeTimeJob {
+                        job.target.performSelector(job.selector)
+                    }
+                })
+            }
+            CFRunLoopAddObserver(runLoop, observer, kCFRunLoopCommonModes)
+        }
+    }
+
     let target: NSObject
     let selector: Selector
 
