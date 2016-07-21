70521fda486be0cc918bb372b110363bc8358d50
nixzhu <zhuhongxu@gmail.com>
2016-07-21 17:02:23 +0800

    clear

--- Yep/ViewControllers/Register/RegisterPickMobileViewController.swift
+++ Yep/ViewControllers/Register/RegisterPickMobileViewController.swift
@@ -78,11 +78,6 @@ final class RegisterPickMobileViewController: SegueViewController {
         }
     }
 
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-
-    }
-
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
 
