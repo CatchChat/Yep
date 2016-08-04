2411ddab541bb6b70dfc0949c3b25b6b25efdefa
nixzhu <zhuhongxu@gmail.com>
2016-08-04 14:56:46 +0800

    previewActionItems for Feeds

--- Yep/ViewControllers/Feeds/FeedsViewController.swift
+++ Yep/ViewControllers/Feeds/FeedsViewController.swift
@@ -821,6 +821,17 @@ final class FeedsViewController: BaseViewController {
         }
     }
 
+    // MARK: - Preview Actions
+
+    override func previewActionItems() -> [UIPreviewActionItem] {
+
+        let subscribeAction = UIPreviewAction(title: NSLocalizedString("Subscribe", comment: ""), style: .Default) { (action, previewViewController) in
+
+        }
+
+        return [subscribeAction]
+    }
+
     // MARK: - Navigation
 
     private var newFeedViewController: NewFeedViewController?
