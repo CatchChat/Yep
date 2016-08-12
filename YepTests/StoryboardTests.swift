edc4204f9f57a9500cd2596036f4399ccaf9c3d0
nixzhu <zhuhongxu@gmail.com>
2016-08-12 10:10:49 +0800

    better testStoryboard

--- YepTests/StoryboardTests.swift
+++ YepTests/StoryboardTests.swift
@@ -14,26 +14,30 @@ final class StoryboardTests: XCTestCase {
     func testStoryboard() {
 
         do {
-            let show = UIStoryboard.yep_show
-            let main = UIStoryboard.yep_main
+            let storyboards: [UIStoryboard] = [
+                UIStoryboard.yep_show,
+                UIStoryboard.yep_main,
+            ]
 
-            print([show, main])
+            print("storyboards: \(storyboards.count)")
         }
 
         do {
-            let a = UIStoryboard.Scene.pickPhotos
-            let b = UIStoryboard.Scene.conversation
-            let c = UIStoryboard.Scene.profile
-            let d = UIStoryboard.Scene.mediaPreview
-            let e = UIStoryboard.Scene.meetGenius
-            let f = UIStoryboard.Scene.discover
-            let g = UIStoryboard.Scene.geniusInterview
-            let h = UIStoryboard.Scene.registerSelectSkills
-            let i = UIStoryboard.Scene.registerPickSkills
-            let j = UIStoryboard.Scene.registerPickName
-            let k = UIStoryboard.Scene.loginByMobile
+            let scenes: [UIViewController] = [
+                UIStoryboard.Scene.pickPhotos,
+                UIStoryboard.Scene.conversation,
+                UIStoryboard.Scene.profile,
+                UIStoryboard.Scene.mediaPreview,
+                UIStoryboard.Scene.meetGenius,
+                UIStoryboard.Scene.discover,
+                UIStoryboard.Scene.geniusInterview,
+                UIStoryboard.Scene.registerSelectSkills,
+                UIStoryboard.Scene.registerPickSkills,
+                UIStoryboard.Scene.registerPickName,
+                UIStoryboard.Scene.loginByMobile,
+            ]
 
-            print([a, b, c, d, e, f, g, h, i, j, k])
+            print("scenes: \(scenes.count)")
         }
     }
 }
