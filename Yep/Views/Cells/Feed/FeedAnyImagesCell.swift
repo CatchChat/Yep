7fd56962bb5f715180ba902502790b9e6914256d
nixzhu <zhuhongxu@gmail.com>
2016-08-16 17:10:07 +0800

    Async

--- Yep/Views/Cells/Feed/FeedAnyImagesCell.swift
+++ Yep/Views/Cells/Feed/FeedAnyImagesCell.swift
@@ -97,54 +97,10 @@ final class FeedAnyImagesCell: FeedBasicCell {
         return node
     }()
 
-    /*
-    lazy var mediaCollectionView: UICollectionView = {
-
-        let layout = UICollectionViewFlowLayout()
-        layout.minimumLineSpacing = 5
-        layout.scrollDirection = .Horizontal
-
-        let collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: layout)
-        collectionView.scrollsToTop = false
-        collectionView.contentInset = UIEdgeInsets(top: 0, left: 15 + 40 + 10, bottom: 0, right: 15)
-        collectionView.showsHorizontalScrollIndicator = false
-        collectionView.backgroundColor = UIColor.clearColor()
-
-        collectionView.registerNibOf(FeedMediaCell)
-
-        collectionView.dataSource = self
-        collectionView.delegate = self
-
-        let backgroundView = TouchClosuresView(frame: collectionView.bounds)
-        backgroundView.touchesBeganAction = { [weak self] in
-            if let strongSelf = self {
-                strongSelf.touchesBeganAction?(strongSelf)
-            }
-        }
-        backgroundView.touchesEndedAction = { [weak self] in
-            if let strongSelf = self {
-                if strongSelf.editing {
-                    return
-                }
-                strongSelf.touchesEndedAction?(strongSelf)
-            }
-        }
-        backgroundView.touchesCancelledAction = { [weak self] in
-            if let strongSelf = self {
-                strongSelf.touchesCancelledAction?(strongSelf)
-            }
-        }
-        collectionView.backgroundView = backgroundView
-
-        return collectionView
-    }()
-     */
-
     var tapImagesAction: FeedTapImagesAction?
 
     var attachments = [DiscoveredAttachment]() {
         didSet {
-            //mediaCollectionView.reloadData()
             mediaCollectionNode.reloadData()
         }
     }
@@ -152,7 +108,6 @@ final class FeedAnyImagesCell: FeedBasicCell {
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
-        //contentView.addSubview(mediaCollectionView)
         contentView.addSubview(mediaCollectionNode.view)
     }
 
@@ -175,7 +130,6 @@ final class FeedAnyImagesCell: FeedBasicCell {
         }
 
         let anyImagesLayout = layout.anyImagesLayout!
-        //mediaCollectionView.frame = anyImagesLayout.mediaCollectionViewFrame
         mediaCollectionNode.frame = anyImagesLayout.mediaCollectionViewFrame
     }
 }
@@ -229,50 +183,3 @@ extension FeedAnyImagesCell: ASCollectionDataSource, ASCollectionDelegate {
     }
 }
 
-/*
-extension FeedAnyImagesCell: UICollectionViewDataSource, UICollectionViewDelegate {
-
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 1
-    }
-
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return attachments.count
-    }
-
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-
-        let cell: FeedMediaCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
-        
-        if let attachment = attachments[safe: indexPath.item] {
-            cell.configureWithAttachment(attachment, bigger: (attachments.count == 1))
-        }
-
-        return cell
-    }
-
-    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize {
-
-        return YepConfig.FeedNormalImagesCell.imageSize
-    }
-
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
-
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-
-        guard let firstAttachment = attachments.first where !firstAttachment.isTemporary else {
-            return
-        }
-
-        let cell = collectionView.cellForItemAtIndexPath(indexPath) as! FeedMediaCell
-
-        let transitionViews: [UIView?] = (0..<attachments.count).map({
-            let cell = collectionView.cellForItemAtIndexPath(NSIndexPath(forItem: $0, inSection: indexPath.section)) as? FeedMediaCell
-            return cell?.imageView
-        })
-        tapImagesAction?(transitionViews: transitionViews, attachments: attachments, image: cell.imageView.image, index: indexPath.item)
-    }
-}
-*/
