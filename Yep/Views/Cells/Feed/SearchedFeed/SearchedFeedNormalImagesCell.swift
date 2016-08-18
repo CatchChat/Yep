3be39c5422a1539d9a839490802be6d8f681ff3a
nixzhu <zhuhongxu@gmail.com>
2016-08-18 10:38:12 +0800

    imageNodes in SearchedFeedNormalImagesCell

--- Yep/Views/Cells/Feed/SearchedFeed/SearchedFeedNormalImagesCell.swift
+++ Yep/Views/Cells/Feed/SearchedFeed/SearchedFeedNormalImagesCell.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import YepKit
+import AsyncDisplayKit
 
 final class SearchedFeedNormalImagesCell: SearchedFeedBasicCell {
 
@@ -20,6 +21,41 @@ final class SearchedFeedNormalImagesCell: SearchedFeedBasicCell {
 
     var tapImagesAction: FeedTapImagesAction?
 
+    private func createImageNode() -> ASImageNode {
+
+        let node = ASImageNode()
+        node.frame = CGRect(origin: CGPointZero, size: YepConfig.FeedNormalImagesCell.imageSize)
+        node.contentMode = .ScaleAspectFill
+        node.backgroundColor = YepConfig.FeedMedia.backgroundColor
+        node.borderWidth = 1
+        node.borderColor = UIColor.yepBorderColor().CGColor
+
+        node.userInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(SearchedFeedNormalImagesCell.tap(_:)))
+        node.view.addGestureRecognizer(tap)
+
+        return node
+    }
+
+    private lazy var imageNode1: ASImageNode = {
+        return self.createImageNode()
+    }()
+
+    private lazy var imageNode2: ASImageNode = {
+        return self.createImageNode()
+    }()
+
+    private lazy var imageNode3: ASImageNode = {
+        return self.createImageNode()
+    }()
+
+    private lazy var imageNode4: ASImageNode = {
+        return self.createImageNode()
+    }()
+
+    private var imageNodes: [ASImageNode] = []
+
+    /*
     private func createImageViewWithFrame(frame: CGRect) -> UIImageView {
         let imageView = UIImageView()
         imageView.contentMode = .ScaleAspectFill
@@ -69,16 +105,31 @@ final class SearchedFeedNormalImagesCell: SearchedFeedBasicCell {
     }()
 
     var imageViews: [UIImageView] = []
+     */
 
+    private let needAllImageNodes: Bool = SearchFeedsViewController.feedNormalImagesCountThreshold == 4
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
+        /*
         contentView.addSubview(imageView1)
         contentView.addSubview(imageView2)
         contentView.addSubview(imageView3)
         contentView.addSubview(imageView4)
 
         imageViews = [imageView1, imageView2, imageView3, imageView4]
+         */
+
+        if needAllImageNodes {
+            imageNodes = [imageNode1, imageNode2, imageNode3, imageNode4]
+        } else {
+            imageNodes = [imageNode1, imageNode2, imageNode3]
+        }
+
+        imageNodes.forEach({
+            contentView.addSubview($0.view)
+        })
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -88,12 +139,75 @@ final class SearchedFeedNormalImagesCell: SearchedFeedBasicCell {
     override func prepareForReuse() {
         super.prepareForReuse()
 
+        /*
         imageView1.image = nil
         imageView2.image = nil
         imageView3.image = nil
         imageView4.image = nil
+         */
+
+        imageNodes.forEach({ $0.image = nil })
     }
 
+    override func configureWithFeed(feed: DiscoveredFeed, layout: SearchedFeedCellLayout, keyword: String?) {
+
+        super.configureWithFeed(feed, layout: layout, keyword: keyword)
+
+        if let attachments = feed.imageAttachments {
+
+            for i in 0..<imageNodes.count {
+
+                if let attachment = attachments[safe: i] {
+
+                    if attachment.isTemporary {
+                        imageNodes[i].image = attachment.image
+
+                    } else {
+                        imageNodes[i].yep_showActivityIndicatorWhenLoading = true
+                        imageNodes[i].yep_setImageOfAttachment(attachment, withSize: YepConfig.FeedNormalImagesCell.imageSize)
+                    }
+
+                    imageNodes[i].hidden = false
+
+                } else {
+                    imageNodes[i].hidden = true
+                }
+            }
+        }
+
+        let normalImagesLayout = layout.normalImagesLayout!
+        imageNode1.frame = normalImagesLayout.imageView1Frame
+        imageNode2.frame = normalImagesLayout.imageView2Frame
+        imageNode3.frame = normalImagesLayout.imageView3Frame
+        if needAllImageNodes {
+            imageNode4.frame = normalImagesLayout.imageView4Frame
+        }
+    }
+
+    // MARK: Actions
+
+    @objc private func tap(sender: UITapGestureRecognizer) {
+
+        guard let attachments = feed?.imageAttachments else {
+            return
+        }
+
+        guard let firstAttachment = attachments.first where !firstAttachment.isTemporary else {
+            return
+        }
+
+        let views = imageNodes.map({ $0.view })
+
+        guard let view = sender.view, index = views.indexOf(view) else {
+            return
+        }
+
+        let transitionViews: [UIView?] = views.map({ $0 })
+        let image = imageNodes[index].image
+        tapImagesAction?(transitionViews: transitionViews, attachments: attachments, image: image, index: index)
+    }
+
+    /*
     override func configureWithFeed(feed: DiscoveredFeed, layout: SearchedFeedCellLayout, keyword: String?) {
 
         super.configureWithFeed(feed, layout: layout, keyword: keyword)
@@ -143,5 +257,6 @@ final class SearchedFeedNormalImagesCell: SearchedFeedBasicCell {
             }
         }
     }
+     */
 }
 
