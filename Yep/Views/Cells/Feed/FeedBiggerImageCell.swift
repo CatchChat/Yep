4a1e7b8fa7bcd349d905be4415f176fab1d63e38
nixzhu <zhuhongxu@gmail.com>
2016-08-18 10:12:41 +0800

    imageNode in FeedBiggerImageCell

--- Yep/Views/Cells/Feed/FeedBiggerImageCell.swift
+++ Yep/Views/Cells/Feed/FeedBiggerImageCell.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import YepKit
+import AsyncDisplayKit
 
 final class FeedBiggerImageCell: FeedBasicCell {
 
@@ -20,6 +21,27 @@ final class FeedBiggerImageCell: FeedBasicCell {
 
     var tapImagesAction: FeedTapImagesAction?
 
+    private func createImageNode() -> ASImageNode {
+
+        let node = ASImageNode()
+        node.frame = CGRect(origin: CGPointZero, size: YepConfig.FeedBiggerImageCell.imageSize)
+        node.contentMode = .ScaleAspectFill
+        node.backgroundColor = YepConfig.FeedMedia.backgroundColor
+        node.borderWidth = 1
+        node.borderColor = UIColor.yepBorderColor().CGColor
+
+        node.userInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(FeedBiggerImageCell.tap(_:)))
+        node.view.addGestureRecognizer(tap)
+
+        return node
+    }
+
+    private lazy var imageNode: ASImageNode = {
+        return self.createImageNode()
+    }()
+
+    /*
     lazy var biggerImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .ScaleAspectFill
@@ -34,11 +56,13 @@ final class FeedBiggerImageCell: FeedBasicCell {
 
         return imageView
     }()
+     */
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
-        contentView.addSubview(biggerImageView)
+        //contentView.addSubview(biggerImageView)
+        contentView.addSubview(imageNode.view)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -48,9 +72,41 @@ final class FeedBiggerImageCell: FeedBasicCell {
     override func prepareForReuse() {
         super.prepareForReuse()
 
-        biggerImageView.image = nil
+        //biggerImageView.image = nil
+        imageNode.image = nil
     }
 
+    override func configureWithFeed(feed: DiscoveredFeed, layout: FeedCellLayout, needShowSkill: Bool) {
+
+        super.configureWithFeed(feed, layout: layout, needShowSkill: needShowSkill)
+
+        if let onlyAttachment = feed.imageAttachments?.first {
+
+            if onlyAttachment.isTemporary {
+                imageNode.image = onlyAttachment.image
+
+            } else {
+                imageNode.yep_showActivityIndicatorWhenLoading = true
+                imageNode.yep_setImageOfAttachment(onlyAttachment, withSize: YepConfig.FeedBiggerImageCell.imageSize)
+            }
+        }
+
+        let biggerImageLayout = layout.biggerImageLayout!
+        imageNode.frame = biggerImageLayout.biggerImageViewFrame
+    }
+
+    @objc private func tap(sender: UITapGestureRecognizer) {
+
+        guard let firstAttachment = feed?.imageAttachments?.first where !firstAttachment.isTemporary else {
+            return
+        }
+
+        if let attachments = feed?.imageAttachments {
+            tapImagesAction?(transitionViews: [imageNode.view], attachments: attachments, image: imageNode.image, index: 0)
+        }
+    }
+
+    /*
     override func configureWithFeed(feed: DiscoveredFeed, layout: FeedCellLayout, needShowSkill: Bool) {
 
         super.configureWithFeed(feed, layout: layout, needShowSkill: needShowSkill)
@@ -80,5 +136,6 @@ final class FeedBiggerImageCell: FeedBasicCell {
             tapImagesAction?(transitionViews: [biggerImageView], attachments: attachments, image: biggerImageView.image, index: 0)
         }
     }
+     */
 }
 
