9e185a781416d600481c2b6f57d5de05cff21e6f
nixzhu <zhuhongxu@gmail.com>
2016-08-29 15:45:12 +0800

    add MobilePhoneReducer

---
+++ Yep/ReSwift/Reducers/MobilePhoneReducer.swift
@@ -0,0 +1,20 @@
+//
+//  MobilePhoneReducer.swift
+//  Yep
+//
+//  Created by NIX on 16/8/29.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import ReSwift
+
+struct MobilePhoneReducer: Reducer {
+
+    func handleAction(action: Action, state: AppState?) -> AppState {
+
+        let state = state ?? AppState()
+
+        return state
+    }
+}
+
