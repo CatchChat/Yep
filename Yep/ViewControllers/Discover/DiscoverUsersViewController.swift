1c98bffeced05ec6a6159ec72f0b3b46f301ecd9
nixzhu <zhuhongxu@gmail.com>
2016-08-29 11:21:29 +0800

    try DiscoverUsersViewController

---
+++ Yep/ViewControllers/Discover/DiscoverUsersViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DiscoverUsersViewController.swift
+//  Yep
+//
+//  Created by NIX on 16/8/29.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+import UIKit
+import ComponentKit
+
+class DiscoverUsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
