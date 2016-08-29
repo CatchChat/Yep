880edcbb084491ae638712c4187916c461d71b4e
nixzhu <zhuhongxu@gmail.com>
2016-08-29 15:35:35 +0800

    add MobilePhone & AppState

---
+++ Yep/ReSwift/MobilePhone.swift
@@ -0,0 +1,14 @@
+//
+//  MobilePhone.swift
+//  Yep
+//
+//  Created by NIX on 16/8/29.
+//  Copyright © 2016年 Catch Inc. All rights reserved.
+//
+
+struct MobilePhone {
+
+    let areaCode: String
+    let number: String
+}
+
