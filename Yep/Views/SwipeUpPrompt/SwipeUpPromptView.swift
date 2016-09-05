6e8f167f1097bb2ea2c55624ae69672f24826bc3
nixzhu <zhuhongxu@gmail.com>
2016-09-05 14:44:41 +0800

    makeUI of SwipeUpPromptView

--- Yep/Views/SwipeUpPrompt/SwipeUpPromptView.swift
+++ Yep/Views/SwipeUpPrompt/SwipeUpPromptView.swift
@@ -8,19 +8,44 @@
 
 import UIKit
 
-class SwipeUpPromptView: UIView {
+final class SwipeUpPromptView: UIView {
 
-    lazy var arrowImageView: UIImageView = {
+    private lazy var arrowImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage.yep_swipeUp
         return imageView
     }()
 
-    lazy var promptLabel: UILabel = {
+    private lazy var promptLabel: UILabel = {
         let label = UILabel()
         label.textColor = UIColor.redColor()
         label.font = UIFont.systemFontOfSize(15)
         return label
     }()
+
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+
+        makeUI()
+    }
+
+    private func makeUI() {
+
+        do {
+            addSubview(promptLabel)
+            promptLabel.translatesAutoresizingMaskIntoConstraints = false
+
+            promptLabel.bottomAnchor.constraintEqualToAnchor(self.bottomAnchor, constant: 20).active = true
+            promptLabel.centerXAnchor.constraintEqualToAnchor(self.centerXAnchor).active = true
+        }
+
+        do {
+            addSubview(arrowImageView)
+            arrowImageView.translatesAutoresizingMaskIntoConstraints = false
+
+            arrowImageView.bottomAnchor.constraintEqualToAnchor(promptLabel.bottomAnchor, constant: 10)
+            arrowImageView.centerXAnchor.constraintEqualToAnchor(promptLabel.centerXAnchor)
+        }
+    }
 }
 
