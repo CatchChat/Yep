4257c9c71fca5f54bdd2db437810826247104a38
nixzhu <zhuhongxu@gmail.com>
2016-09-22 10:11:24 +0800

    fix FreeTimeJob

--- Yep/RunLoop/FreeTimeJob.swift
+++ Yep/RunLoop/FreeTimeJob.swift
@@ -10,30 +10,30 @@ import Foundation
 
 class FreeTimeJob {
 
-    private static var __once: () = {
-            let runLoop = CFRunLoopGetMain()
-            let observer: CFRunLoopObserver = CFRunLoopObserverCreateWithHandler(kCFAllocatorDefault, CFRunLoopActivity.beforeWaiting.rawValue | CFRunLoopActivity.exit.rawValue, true, 0xFFFFFF) { (observer, activity) in
-                guard set.count != 0 else {
-                    return
-                }
-
-                let currentSet = set
-                set = NSMutableSet()
-
-                currentSet.enumerateObjects({ (object, stop) in
-                    if let job = object as? FreeTimeJob {
-                        job.target?.perform(job.selector)
-                    }
-                })
+    private static var once: Void = {
+        let runLoop = CFRunLoopGetMain()
+        let observer: CFRunLoopObserver = CFRunLoopObserverCreateWithHandler(kCFAllocatorDefault, CFRunLoopActivity.beforeWaiting.rawValue | CFRunLoopActivity.exit.rawValue, true, 0xFFFFFF) { (observer, activity) in
+            guard set.count != 0 else {
+                return
             }
-            CFRunLoopAddObserver(runLoop, observer, CFRunLoopMode.commonModes)
-        }()
+
+            let currentSet = set
+            set = NSMutableSet()
+
+            currentSet.enumerateObjects({ (object, stop) in
+                if let job = object as? FreeTimeJob {
+                    _ = job.target?.perform(job.selector)
+                }
+            })
+        }
+        CFRunLoopAddObserver(runLoop, observer, CFRunLoopMode.commonModes)
+    }()
 
     fileprivate static var set = NSMutableSet()
 
     fileprivate static var onceToken: Int = 0
     fileprivate class func setup() {
-        _ = FreeTimeJob.__once
+        _ = FreeTimeJob.once
     }
 
     fileprivate weak var target: NSObject?
