e327060c2f886c2f7b6008f85251eadc6455be6e
nixzhu <zhuhongxu@gmail.com>
2016-09-27 15:44:45 +0800

    do not extension PhoneNumberRepresentation for UITextFieldDelegate (kill warning in Swift 3)

--- Yep/Base/ViewControllers/BaseInputMobileViewController.swift
+++ Yep/Base/ViewControllers/BaseInputMobileViewController.swift
@@ -26,5 +26,34 @@ class BaseInputMobileViewController: BaseViewController, PhoneNumberRepresentati
     func tappedKeyboardReturn() {
         assert(false, "Must override tappedKeyboardReturn")
     }
+
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+
+        if textField == areaCodeTextField {
+            adjustAreaCodeTextFieldWidth()
+        }
+
+        return true
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+
+        if textField == areaCodeTextField {
+            UIView.animate(withDuration: 0.1, delay: 0.0, options: UIViewAnimationOptions(), animations: { [weak self] _ in
+                self?.areaCodeTextFieldWidthConstraint.constant = 60
+                self?.view.layoutIfNeeded()
+            }, completion: nil)
+        }
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+        guard let areaCode = areaCodeTextField.text , !areaCode.isEmpty else { return true }
+        guard let number = mobileNumberTextField.text , !number.isEmpty else { return true }
+        
+        tappedKeyboardReturn()
+        
+        return true
+    }
 }
 
--- Yep/Protocols/PhoneNumberRepresentation.swift
+++ Yep/Protocols/PhoneNumberRepresentation.swift
@@ -35,35 +35,3 @@ extension PhoneNumberRepresentation where Self: UIViewController {
     }
 }
 
-extension PhoneNumberRepresentation where Self: UIViewController {
-
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-
-        if textField == areaCodeTextField {
-            adjustAreaCodeTextFieldWidth()
-        }
-
-        return true
-    }
-
-    func textFieldDidEndEditing(_ textField: UITextField) {
-
-        if textField == areaCodeTextField {
-            UIView.animate(withDuration: 0.1, delay: 0.0, options: UIViewAnimationOptions(), animations: { [weak self] _ in
-                self?.areaCodeTextFieldWidthConstraint.constant = 60
-                self?.view.layoutIfNeeded()
-            }, completion: nil)
-        }
-    }
-
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-
-        guard let areaCode = areaCodeTextField.text , !areaCode.isEmpty else { return true }
-        guard let number = mobileNumberTextField.text , !number.isEmpty else { return true }
-
-        tappedKeyboardReturn()
-
-        return true
-    }
-}
-
